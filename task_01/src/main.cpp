68b8b4b0f1cc32924d6cb54b840faee3cb02c66d
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-02-19 22:51:49 +0000

    create main algo

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -1,3 +1,41 @@
+// std libs:
+#include <exception>
 #include <iostream>
+#include <vector>
 
-int main() { return 0; }
+// my libs:
+#include "two_sum.hpp"
+#include "utilities.hpp"
+
+// std usings:
+using std::cout, std::cerr, std::endl;
+
+int main() {
+  cout << "That is the programm, that finds 2 numbers" << endl
+       << "from sorted array that add up to your given number." << endl;
+
+  long long number;
+  ReadNumberFromConsole(number);
+
+  // MEANS: вектор, который вводит пользователь
+  std::vector<long long> vec{0, -1};
+
+  while (!IsSorted(vec)) {
+    ReadFromConsole(vec);
+    if (!IsSorted(vec)) cout << "Please, enter sorted array." << endl;
+  }
+
+  try {
+    // MEANS: пара чисел из вектора, которые в сумме дадут заданное число
+    auto need_pair = TwoElemsGivingSum(vec, number);
+
+    cout << "Given number is equal to the sum: ";
+    PrintToConsole(need_pair);
+  } catch (const std::exception& e) {
+    cerr << e.what() << endl;
+  }
+
+  cout << endl;
+
+  return 0;
+}
