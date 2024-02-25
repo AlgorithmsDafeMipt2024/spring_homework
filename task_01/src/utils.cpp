c8700d97837897e8486623c8ef01e39776ff78e2
Matvey-cmd <143873323+Matvey-cmd@users.noreply.github.com>
2024-02-25 18:16:21 +0000

    Right_Test

--- task_01/src/utils.cpp
+++ task_01/src/utils.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 
 std::pair<int, int> Task1(int num, const std::vector<int> arr) {
+  if (arr.size() < 2) {
+    throw WrongVector("");
+  }
   for (int i = 0, j = arr.size() - 1; i < j;) {
     auto sum = arr[i] + arr[j];
     if (sum < num)
