33a4b08b3c815995cbc3e41c5ee94e40771dab90
sh1buya <92878896+sh1buya@users.noreply.github.com>
2024-03-03 14:42:49 +0000

    QuickSort done

--- task_sortings/src/sortings.cpp
+++ task_sortings/src/sortings.cpp
@@ -217,3 +217,40 @@ std::vector<int> MergeSort_bottomup(std::vector<int> massive) {
   }
   return parts[0];
 }
+
+// O*(NlogN) (but upper limit is O(n^2) if pivat is bad)
+std::vector<int> QuickSort(std::vector<int> massive) {
+  int pivat_index = massive.size() / 2;
+  int pivat = massive[pivat_index];
+
+  std::vector<int> smaller_part;
+  std::vector<int> bigger_part;
+
+  if (massive.size() > 1) {
+    for (int i = 0; i < massive.size(); i++) {
+      if (i != pivat_index) {
+        if (massive[i] <= pivat) {
+          smaller_part.push_back(massive[i]);
+        } else {
+          bigger_part.push_back(massive[i]);
+        }
+      }
+    }
+  } else {
+    return massive;
+  }
+  if (smaller_part.size() > 1) {
+    smaller_part = QuickSort(smaller_part);
+  }
+
+  if (bigger_part.size() > 1) {
+    bigger_part = QuickSort(bigger_part);
+  }
+
+  std::vector<int> result = smaller_part;
+  result.push_back(pivat);
+  for (int i = 0; i < bigger_part.size(); i++) {
+    result.push_back(bigger_part[i]);
+  }
+  return result;
+}
