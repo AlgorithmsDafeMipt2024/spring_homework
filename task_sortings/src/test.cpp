376e5ddd5c014a6f998bfc9154516215c3a07d0d
sh1buya <92878896+sh1buya@users.noreply.github.com>
2024-03-03 15:18:44 +0000

    correct building checked; 1 test added

--- task_sortings/src/test.cpp
+++ task_sortings/src/test.cpp
@@ -1,8 +1,15 @@
 
 #include <gtest/gtest.h>
 
-#include "sortings.hpp"
+#include "bogo_sort.h"
+#include "bubble_sort.h"
+#include "insertion_sort.h"
+#include "merge_sort.h"
+#include "quick_sort.h"
+#include "selection_sort.h"
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+// BogoSort tests
+TEST(BogoSort, Simple) {
+  std::vector<int> mas = {1};
+  ASSERT_EQ({1}, {1});
 }
