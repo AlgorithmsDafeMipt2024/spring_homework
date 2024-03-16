978668ba682b57a75ecc789dae35dc7e33dd0234
Matvey-cmd <143873323+Matvey-cmd@users.noreply.github.com>
2024-03-16 08:36:08 +0000

    some changes

--- task_01/src/utils.hpp
+++ task_01/src/utils.hpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <vector>
 
-std::pair<int, int> Task1(int num, const std::vector<int> arr);
 class WrongVector : public std::runtime_error {
   using std::runtime_error::runtime_error;
 };
+
+std::pair<int, int> Task1(int num, const std::vector<int> arr);
