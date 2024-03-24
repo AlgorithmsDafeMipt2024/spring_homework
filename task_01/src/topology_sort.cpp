f5dd633dd263760d503b831955ecfb9405f2e981
gulyonatyoma <143820164+gulyonatyoma@users.noreply.github.com>
2024-03-24 18:49:09 +0000

    First_task(changed)

--- task_01/src/topology_sort.cpp
+++ task_01/src/topology_sort.cpp
@@ -1,16 +1,15 @@
 #include "topology_sort.hpp"
 
-pair<long long, long long> solve(vector<long long> a, long long sum) {
-    long long n = a.size();
-    long long i = 0, j = n - 1;
-    while(i < j) {
-        if (a[i] + a[j] == sum) {
-            return {a[i], a[j]};
-        } else if (a[i] + a[j] > sum) {
-            j--;
-        } else {
-            i++;
-        }
+pair<long long, long long> solve(vector<long long> arr, long long sum) {
+  long long first_pointer = 0, second_pointer = arr.size() - 1;
+  while (first_pointer < second_pointer) {
+    if (arr[first_pointer] + arr[second_pointer] == sum) {
+      return {arr[first_pointer], arr[second_pointer]};
+    } else if (arr[first_pointer] + arr[second_pointer] > sum) {
+      second_pointer--;
+    } else {
+      first_pointer++;
     }
-    return {-1, -1};
+  }
+  return {-1, -1};
 }
