b288073f35de442cdc297d55054bab07b9c48047
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-03-24 18:19:36 +0000

    fix bugs by removing using unique_ptr and primary test in main

--- sandbox/stack/src/main.cpp
+++ sandbox/stack/src/main.cpp
@@ -1,3 +1,126 @@
+#include <algorithm>
+#include <deque>
 #include <iostream>
+using std::endl, std::cout;
 
-int main() { return 0; }
+#include "stack.hpp"
+
+int main() {
+  auto stack = Stack<int>();
+  stack.Push(6);
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << "size:" << stack.Size() << endl;
+  cout << endl;
+
+  cout << endl;
+  stack.PushRange({4, 3, 2, 1});
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << "size:" << stack.Size() << endl;
+  cout << endl;
+
+  stack.PushRange(std::vector(4, 0));
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << "size:" << stack.Size() << endl;
+  cout << endl;
+
+  stack.PushRange(std::vector({4, 3, 2, 1}));
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << "size:" << stack.Size() << endl;
+  cout << endl;
+
+  std::stack<int> std_stack;
+  std_stack.push(4);
+  std_stack.push(3);
+  std_stack.push(2);
+  std_stack.push(1);
+  stack.PushRange(std_stack);
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << "size:" << stack.Size() << endl;
+  cout << endl;
+
+  Stack<int> my_stack;
+  my_stack.Push(4);
+  my_stack.Push(3);
+  my_stack.Push(2);
+  my_stack.Push(1);
+  cout << my_stack.Top() << endl;
+  stack.PushRange(my_stack);
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << "size:" << stack.Size() << endl;
+  cout << endl;
+
+  cout << my_stack.Top() << endl;
+  stack = my_stack;
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << stack.Top() << endl;
+  cout << "size:" << stack.Size() << endl;
+  stack.Pop();
+  cout << "size:" << stack.Size() << endl;
+  cout << endl;
+  return 0;
+}
--- sandbox/stack/src/stack.hpp
+++ sandbox/stack/src/stack.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <initializer_list>
+#include <iostream>
 #include <memory>
 #include <stack>
 #include <vector>
@@ -12,12 +13,19 @@
 template <typename T>
 class Elem {
  public:
-  Elem() = default;
-  Elem(Elem*, const T&);
-  Elem(Elem*);
+  Elem() : prev{nullptr} {}
+  ~Elem() { delete prev; }
+
+  Elem(const Elem<T>& elem) : prev{&elem} {}
+  Elem(Elem<T>* elem) : prev{elem} {}
+  Elem(Elem<T>* elem, const T& value) : prev(elem), data{value} {}
+  Elem(const T& value) : prev{nullptr}, data{value} {}
+
+  Elem<T>& operator=(Elem<T>&&) = default;
+  Elem<T>& operator=(const Elem<T>& elem) = default;
 
   /// @brief указатель на предыдущий элемент
-  std::unique_ptr<Elem> prev;
+  Elem<T>* prev;
 
   /// @brief значение элемента
   T data;
@@ -26,21 +34,34 @@ class Elem {
 template <typename T>
 class Stack {
  public:
-  Stack() : top_{nullptr} {}
-  Stack(const Elem<T>&);
+  Stack() : top_{nullptr}, size_{0} {}
+  ~Stack() { delete top_; }
+
   Stack(const Stack<T>&) = default;
-  Stack(const std::stack<T>&);
-  Stack(const std::vector<T>&);
-  Stack(const std::initializer_list<T>&);
+
+  Stack(const T& value) : top_{new Elem<T>(value)} {}
+
+  Stack(const std::stack<T>& stack) {
+    for (const auto& elem : stack) Push(elem);
+  }
+
+  Stack(const std::vector<T>& vector) {
+    for (const auto& elem : vector) Push(elem);
+  }
+
+  Stack(const std::initializer_list<T>& init_list) {
+    for (const auto& elem : init_list) Push(elem);
+  }
 
   Stack& operator=(const Stack<T>&) = default;
+  Stack& operator=(Stack<T>&&) = default;
 
   /**
    * @brief
    * возвращает верхний элемент стека
    * @return T
    */
-  T Top();
+  T Top() { return top_->data; }
 
   /**
    * @brief
@@ -48,54 +69,75 @@ class Stack {
    * @return true: если пуст
    * @return false: если не пуст
    */
-  bool Empty();
+  bool Empty() { return top_ == nullptr; }
 
   /**
    * @brief
    * возвращает размер стека
    * @return size_t
    */
-  size_t Size();
+  size_t Size() { return size_; }
 
   /**
    * @brief
    * добавляет новый элемент в стек
    * @param value: значение нового элемента
    */
-  void Push(const T& value);
+  void Push(const T& value) {
+    auto new_elem = new Elem<T>(top_, value);
+    top_ = std::move(new_elem);
+    size_++;
+  }
 
   /**
    * @brief
    * расширяет стек значениями элементов другого стека
    * @param stack
    */
-  void PushRange(Stack<T> stack);
+  void PushRange(Stack<T> stack) {
+    while (!stack.Empty()) {
+      Push(stack.Top());
+      stack.Pop();
+    }
+  }
 
   /**
    * @brief
    * расширяет стек значениями std::stack
    * @param stack
    */
-  void PushRange(std::stack<T> stack);
+  void PushRange(std::stack<T> stack) {
+    while (!stack.empty()) {
+      Push(stack.top());
+      stack.pop();
+    }
+  }
 
   /**
    * @brief
    * расширяет стек значениями элементов вектора
    * @param vector
    */
-  void PushRange(const std::vector<T>& vector);
+  void PushRange(const std::vector<T>& vector) {
+    for (const auto& elem : vector) Push(elem);
+  }
 
   /**
    * @brief
    * расширяет стек значениями элементов списка инициализации
    * @param init_list
    */
-  void PushRange(const std::initializer_list<T>& init_list);
+  void PushRange(const std::initializer_list<T>& init_list) {
+    for (const auto& elem : init_list) Push(elem);
+  }
 
   /**
    * @return T: верхний элемент стека
    */
-  T Pop();
+  void Pop() {
+    top_ = top_->prev;
+    size_--;
+  }
 
   /**
    * @brief
@@ -106,6 +148,6 @@ class Stack {
 
  private:
   /// @brief указатель на последний элемент
-  std::unique_ptr<Elem<T>> top_;
+  Elem<T>* top_;
   size_t size_;
 };
