d36dde4ccaa8f36daea98f446b16d80a8728f701
Matvey-cmd <143873323+Matvey-cmd@users.noreply.github.com>
2024-03-25 10:50:27 +0000

    k_stat

--- task_06/src/test.cpp
+++ task_06/src/test.cpp
@@ -1,6 +1,11 @@
-
+#include "k_stat.hpp"
 #include <gtest/gtest.h>
+#include <stdexcept>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+TEST(k_stat, Simple) {
+  std::vector<int> vec{14, -1, 5, 21, 6, -3, 7, 93, 9, 0, 84};
+  ASSERT_EQ(Quick_Sort(vec, 0, 10, 8), 14);
+  ASSERT_EQ(Quick_Sort(vec, 0, 10, 3), 0);
+  ASSERT_EQ(Quick_Sort(vec, 0, 10, 10), 84);
+  EXPECT_THROW(Quick_Sort(vec, 0, 10, 13), std::logic_error);
 }
