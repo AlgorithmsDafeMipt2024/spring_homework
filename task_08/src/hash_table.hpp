71846b0b3c5b5fb97d6ecef5b59dd8c39d8a7766
dfsavffc <144057175+dfsavffc@users.noreply.github.com>
2024-04-02 18:10:17 +0000

    Complete task_08

--- task_08/src/hash_table.hpp
+++ task_08/src/hash_table.hpp
@@ -1,31 +1,214 @@
 #pragma once
-#include <list>
-#include <vector>
 
-class HashSet {
-  std::vector<std::list<int>> hash_table;
-  size_t size;
-  constexpr static size_t default_size = 128;
+#include <math.h>
+
+#include <concepts>
+#include <functional>
+#include <memory>
+#include <stdexcept>
+#include <type_traits>
+
+template <typename T>
+class HashTable {
+ public:
+  explicit HashTable(std::function<size_t(T key)> hash_func)
+      : used_cell_count_(0),
+        size_(0),
+        buffer_size_(DefaultBufferSize),
+        table_container_(std::vector<T>(buffer_size_)),
+        cell_conditions_(
+            std::vector<Condition>(buffer_size_, Condition::Empty)),
+        is_custom_hash_func_(true),
+        CustomHashFunction(hash_func) {}
+
+  HashTable()
+      : used_cell_count_(0),
+        size_(0),
+        buffer_size_(DefaultBufferSize),
+        table_container_(std::vector<T>(buffer_size_)),
+        cell_conditions_(
+            std::vector<Condition>(buffer_size_, Condition::Empty)),
+        is_custom_hash_func_(false) {}
+
+  bool Contains(T value) const;
+
+  void Insert(T value);
+
+  void Remove(T value);
+
+  void Clear();
+
+  size_t Size() const;
+
+ private:
+  enum class Condition { Fill, Deleted, Empty };
+  constexpr static const double RehashCoefficient = 0.7;
+  constexpr static const double HashCoefficient = 0.618033989;
+  constexpr static const double DefaultBufferSize = 16;
+  bool is_custom_hash_func_;
+
+  size_t buffer_size_;
+  size_t size_;
+  unsigned used_cell_count_;
+
+  std::vector<T> table_container_;
+  std::vector<Condition> cell_conditions_;
+
+  size_t SecondHashFunction(T key) const;
+
+  size_t FirstHashFunction(T key) const;
+
+  void Rehash();
 
   void ReSize();
 
-  unsigned HashFunction(int value) const;
-
-  void ReHash();
-
- public:
-  HashSet() {
-    size = default_size;
-    hash_table.resize(size, std::list<int>{});
-  };
-
-  bool Contains(int value) const;
-
-  void Insert(int value);
-
-  void Remove(int value);
-
-  size_t Size();
-
-  void Clear();
+  std::function<size_t(T key)> CustomHashFunction;
 };
+
+template <typename T>
+size_t HashTable<T>::Size() const {
+  return size_;
+}
+
+template <typename T>
+concept tip = std::same_as<T, int>;
+
+template <typename T>
+size_t HashTable<T>::SecondHashFunction(T key) const {
+  if constexpr (std::is_arithmetic<T>::value)
+    return (key * buffer_size_ - 1) % buffer_size_;
+  throw std::invalid_argument(
+      "HashHashTable does not support this data type, use custom hash "
+      "functions");
+}
+
+template <typename T>
+size_t HashTable<T>::FirstHashFunction(T key) const {
+  if constexpr (std::is_arithmetic<T>::value)
+    return floor(buffer_size_ *
+                 ((key * HashCoefficient) - floor(key * HashCoefficient)));
+  throw std::invalid_argument(
+      "HashHashTable does not support this data type, use custom hash "
+      "functions");
+}
+
+template <typename T>
+void HashTable<T>::Clear() {
+  size_ = 0;
+  used_cell_count_ = 0;
+  std::fill(cell_conditions_.begin(), cell_conditions_.end(), Condition::Empty);
+}
+
+template <typename T>
+void HashTable<T>::Insert(T value) {
+  size_t hash;
+  if (is_custom_hash_func_) {
+    hash = CustomHashFunction(value) % buffer_size_;
+    while (cell_conditions_[hash] == Condition::Fill) {
+      if (table_container_[hash] == value) return;
+      hash++;
+    }
+  } else {
+    hash = FirstHashFunction(value) % buffer_size_;
+    int attempt_count = 0;
+    while (cell_conditions_[hash] == Condition::Fill) {
+      if (table_container_[hash] == value) return;
+      attempt_count++;
+      hash = (FirstHashFunction(value) +
+              attempt_count * SecondHashFunction(value)) %
+             buffer_size_;
+    }
+  }
+
+  table_container_[hash] = value;
+  cell_conditions_[hash] = Condition::Fill;
+  used_cell_count_++;
+  size_++;
+
+  auto fill_coefficient = double(used_cell_count_) / buffer_size_;
+  if (fill_coefficient >= RehashCoefficient) Rehash();
+}
+
+template <typename T>
+bool HashTable<T>::Contains(T value) const {
+  size_t hash;
+  if (is_custom_hash_func_) {
+    hash = CustomHashFunction(value) % buffer_size_;
+    while (cell_conditions_[hash] != Condition::Empty) {
+      if (table_container_[hash] == value &&
+          cell_conditions_[hash] == Condition::Fill)
+        return true;
+      hash++;
+    }
+  } else {
+    hash = FirstHashFunction(value) % buffer_size_;
+    int attempt_count = 0;
+    while (cell_conditions_[hash] != Condition::Empty) {
+      if (table_container_[hash] == value &&
+          cell_conditions_[hash] == Condition::Fill)
+        return true;
+      attempt_count++;
+      hash = (FirstHashFunction(value) +
+              attempt_count * SecondHashFunction(value)) %
+             buffer_size_;
+    }
+  }
+  return false;
+}
+
+template <typename T>
+void HashTable<T>::Remove(T value) {
+  size_t hash;
+  if (is_custom_hash_func_) {
+    hash = CustomHashFunction(value);
+    while (cell_conditions_[hash] != Condition::Empty) {
+      if (table_container_[hash] == value &&
+          cell_conditions_[hash] == Condition::Fill) {
+        cell_conditions_[hash] = Condition::Deleted;
+        size_--;
+        break;
+      }
+      if (table_container_[hash] == value &&
+          cell_conditions_[hash] == Condition::Deleted)
+        break;
+      hash++;
+    }
+  } else {
+    hash = FirstHashFunction(value) % buffer_size_;
+    int attempt_count = 0;
+    while (cell_conditions_[hash] != Condition::Empty) {
+      if (table_container_[hash] == value &&
+          cell_conditions_[hash] == Condition::Fill) {
+        cell_conditions_[hash] = Condition::Deleted;
+        size_--;
+        break;
+      }
+      if (table_container_[hash] == value &&
+          cell_conditions_[hash] == Condition::Deleted)
+        break;
+      attempt_count++;
+      hash = (FirstHashFunction(value) +
+              attempt_count * SecondHashFunction(value)) %
+             buffer_size_;
+    }
+  }
+}
+
+template <typename T>
+void HashTable<T>::ReSize() {
+  buffer_size_ *= 2;
+  table_container_.resize(buffer_size_);
+  cell_conditions_.resize(buffer_size_);
+}
+
+template <typename T>
+void HashTable<T>::Rehash() {
+  std::vector<T> outdated_container;
+  for (int i = 0; i < buffer_size_; ++i)
+    if (cell_conditions_[i] == Condition::Fill)
+      outdated_container.push_back(table_container_[i]);
+
+  ReSize();
+  Clear();
+  for (auto &elem : outdated_container) Insert(elem);
+}
--- task_08/src/test.cpp
+++ task_08/src/test.cpp
@@ -1,40 +1,93 @@
 
-// #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-// #include "hash_table.hpp"
+#include <cstddef>
+#include <string>
 
-// TEST(hash_table, Simple) {
-//   HashSet set;
-//   set.Insert(14);
-//   set.Insert(124);
-//   set.Insert(1);
-//   set.Insert(-23);
-//   set.Insert(54);
-//   set.Insert(44);
-//   set.Insert(0);
-//   set.Insert(674);
+#include "hash_table.hpp"
 
-//   ASSERT_EQ(set.Contains(124), true);
-//   ASSERT_EQ(set.Contains(44), true);
-//   ASSERT_EQ(set.Contains(54), true);
-//   ASSERT_EQ(set.Contains(0), true);
-//   ASSERT_EQ(set.Contains(-23), true);
-//   ASSERT_EQ(set.Contains(1), true);
-//   ASSERT_EQ(set.Contains(1), true);
+size_t CustomHashFunction(int key) { return key % 10; }
+size_t CustomStringHashFunction(std::string key) { return key.size() % 17; }
 
-//   ASSERT_EQ(set.Contains(14), true);
-//   set.Remove(14);
-//   ASSERT_EQ(set.Contains(14), false);
-//   set.Remove(14);
-//   ASSERT_EQ(set.Contains(14), false);
+TEST(HashTableTest, SimpleTest) {
+  HashTable<int> table;
+  ASSERT_EQ(table.Size(), 0);
 
-//   set.Clear();
-//   ASSERT_EQ(set.Contains(0), false);
+  table.Insert(14);
+  table.Insert(124);
+  table.Insert(1);
+  table.Insert(-23);
+  table.Insert(54);
+  table.Insert(44);
+  table.Insert(0);
+  table.Insert(674);
 
-//   for (int i = 0; i < 100000; ++i) set.Insert(i);
-//   ASSERT_EQ(set.Contains(18353), true);
-//   set.Remove(18353);
-//   ASSERT_EQ(set.Contains(18353), false);
-//   set.Insert(-99999);
-//   set.Contains(-99999);
-// }
+  ASSERT_EQ(table.Contains(124), true);
+  ASSERT_EQ(table.Contains(44), true);
+  ASSERT_EQ(table.Contains(54), true);
+  ASSERT_EQ(table.Contains(0), true);
+  ASSERT_EQ(table.Contains(-23), true);
+  ASSERT_EQ(table.Contains(1), true);
+
+  ASSERT_EQ(table.Size(), 8);
+
+  table.Remove(14);
+  ASSERT_EQ(table.Contains(14), false);
+
+  table.Clear();
+  ASSERT_EQ(table.Size(), 0);
+
+  for (int i = 0; i < 100000; ++i) {
+    table.Insert(i);
+  }
+
+  ASSERT_EQ(table.Contains(18353), true);
+  table.Remove(18353);
+  ASSERT_EQ(table.Contains(18353), false);
+
+  table.Insert(-99999);
+  ASSERT_EQ(table.Contains(-99999), true);
+}
+
+TEST(HashTableTest, CustomHashFunctionTest) {
+  HashTable<int> table(CustomHashFunction);
+
+  table.Insert(10);
+  table.Insert(26);
+
+  ASSERT_EQ(table.Contains(10), true);
+  ASSERT_EQ(table.Contains(26), true);
+
+  table.Remove(10);
+  table.Remove(26);
+
+  ASSERT_EQ(table.Contains(10), false);
+  ASSERT_EQ(table.Contains(26), false);
+}
+
+TEST(HashTableTest, CollisionTest) {
+  HashTable<int> table(CustomHashFunction);
+
+  table.Insert(10);
+  table.Insert(20);
+  table.Insert(30);
+
+  ASSERT_EQ(table.Contains(10), true);
+  ASSERT_EQ(table.Contains(20), true);
+  ASSERT_EQ(table.Contains(30), true);
+}
+
+TEST(HashTableTest, AnotherTypeTest) {
+  HashTable<std::string> table(CustomStringHashFunction);
+
+  table.Insert("Dog");
+  table.Insert("Cat");
+  table.Insert("Very long string");
+  ASSERT_EQ(table.Contains("Dog"), true);
+  ASSERT_EQ(table.Contains("Dog"), true);
+  ASSERT_EQ(table.Contains("Short string"), false);
+
+  table.Remove("Dog");
+  ASSERT_EQ(table.Contains("Dog"), false);
+  ASSERT_EQ(table.Size(), 2);
+}
