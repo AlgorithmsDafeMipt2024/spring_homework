13037648d021a0285d96984d256ffce65675ca27
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-07 23:46:25 +0000

    restore deleted task

---
+++ task_02/src/min_stack/min_singly_list_stack.hpp
@@ -0,0 +1,261 @@
+#pragma once
+
+#include <initializer_list>
+#include <memory>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+/**
+ * @brief элемент стека (звено в односвязном списке)
+ * @tparam T: тип значения в стеке
+ */
+template <typename T>
+class SinglyListElem {
+ public:
+  SinglyListElem() : prev{nullptr} {}
+
+  SinglyListElem(const SinglyListElem<T>& elem) : prev{&elem} {}
+
+  SinglyListElem(SinglyListElem<T>* elem) : prev{elem} {}
+
+  SinglyListElem(SinglyListElem<T>* elem, const T& value)
+      : prev(elem), data{value} {}
+
+  SinglyListElem(const std::shared_ptr<SinglyListElem<T>>& elem, const T& value)
+      : prev(elem), data{value} {}
+
+  SinglyListElem(const T& value) : prev{nullptr}, data{value} {}
+
+  SinglyListElem<T>& operator=(SinglyListElem<T>&&) = default;
+  SinglyListElem<T>& operator=(const SinglyListElem<T>& elem) = default;
+
+  // @brief указатель на предыдущий элемент
+  std::shared_ptr<SinglyListElem<T>> prev;
+
+  // @brief значение элемента
+  T data;
+};
+
+/**
+ * @brief реализация стека на односвязном списке
+ * @tparam T: тип значений элементов стека
+ */
+template <typename T>
+class MinSinglyListStack {
+ public:
+  MinSinglyListStack() = default;
+
+  MinSinglyListStack(const T& value) { Push(value); }
+
+  MinSinglyListStack(const MinSinglyListStack<T>& stack) = default;
+
+  MinSinglyListStack(const std::stack<T>& stack) { PushRange(stack); }
+
+  MinSinglyListStack(const std::vector<T>& vector) { PushRange(vector); }
+
+  MinSinglyListStack(const std::initializer_list<T>& init_list) {
+    PushRange(init_list);
+  }
+
+  MinSinglyListStack& operator=(const MinSinglyListStack<T>& stack) {
+    PushRange(stack);
+  }
+
+  MinSinglyListStack& operator=(MinSinglyListStack<T>&& stack) {
+    PushRange(stack);
+  }
+
+  /**
+   * @return T: верхний элемент стека
+   * @throw std::logic_error: если стек пуст
+   */
+  T Top() {
+    if (Empty()) throw std::logic_error("Stack is empty");
+
+    return top_->data;
+  }
+
+  /**
+   * @param key: ключ, по которому можно получить Top в виде указателя
+   * @return std::shared_ptr<SinglyListElem<int>>&: ptr на верхний элемент стека
+   * @throw std::logic_error: если ключ != "ptr"
+   */
+  std::shared_ptr<SinglyListElem<int>>& Top(const std::string& key) {
+    if (Empty()) throw std::logic_error("Stack is empty");
+
+    if (key == "ptr")
+      return top_;
+    else
+      throw std::logic_error(
+          "invalid key string, do you mean 'T& Top('ptr')'?");
+  }
+
+  /**
+   * @brief проверяет, пуст ли стек
+   * @return true: если пуст
+   * @return false: если не пуст
+   */
+  bool Empty() { return top_ == nullptr; }
+
+  /**
+   * @return size_t: размер стека
+   */
+  size_t Size() { return size_; }
+
+  /**
+   * @brief добавляет новый элемент в стек
+   * @param value: значение нового элемента
+   */
+  void Push(const T& value = T()) {
+    if (!is_min_init) {
+      min_ = value;
+      is_min_init = true;
+    }
+
+    if (value < min_) min_ = value;
+
+    auto new_elem = std::make_shared<SinglyListElem<T>>(top_, value);
+    top_ = new_elem;
+    size_++;
+  }
+
+  /**
+   * @brief расширяет стек значениями элементов другого стека
+   * @param stack
+   */
+  void PushRange(MinSinglyListStack<T> stack) {
+    MinSinglyListStack<T> temp_stack;
+    while (!stack.Empty()) {
+      temp_stack.Push(stack.Top());
+      stack.Pop();
+    }
+
+    while (!temp_stack.Empty()) {
+      Push(temp_stack.Top());
+      temp_stack.Pop();
+    }
+  }
+
+  /**
+   * @brief расширяет стек значениями std::stack
+   * @param stack
+   */
+  void PushRange(std::stack<T> stack) {
+    std::stack<T> temp_stack;
+    while (!stack.empty()) {
+      temp_stack.push(stack.top());
+      stack.pop();
+    }
+
+    while (!temp_stack.empty()) {
+      Push(temp_stack.top());
+      temp_stack.pop();
+    }
+  }
+
+  /**
+   * @brief расширяет стек значениями элементов вектора
+   * @param vector
+   */
+  void PushRange(const std::vector<T>& vector) {
+    for (const auto& elem : vector) Push(elem);
+  }
+
+  /**
+   * @brief расширяет стек значениями элементов списка инициализации
+   * @param init_list
+   */
+  void PushRange(const std::initializer_list<T>& init_list) {
+    for (const auto& elem : init_list) Push(elem);
+  }
+
+  /**
+   * @throw std::invalid_argument: если стек пуст
+   */
+  void Pop() {
+    if (Empty()) throw std::logic_error("Stack is empty");
+
+    top_ = top_->prev;
+    size_--;
+
+    if (Size() > 0)
+      FindNewMin();
+    else
+      is_min_init = false;
+  }
+
+  /**
+   * @brief меняет все элементы двух стеков местами
+   * @param another_stack
+   * @throw std::invalid_argument: если размеры стеков не совпадает
+   */
+  void Swap(MinSinglyListStack<T>& another_stack) {
+    std::swap(another_stack.Top("ptr"), top_);
+  }
+
+  /**
+   * @brief меняет все элементы двух стеков местами
+   * @param another_stack
+   * @throw std::invalid_argument: если размеры стеков не совпадает
+   */
+  void Swap(std::stack<T>& another_stack) {
+    // проще особо не сделаешь, если хочется сохранить порядок
+
+    MinSinglyListStack<T> extra_stack_1;
+    MinSinglyListStack<T> extra_stack_2;
+
+    while (!Empty()) {
+      extra_stack_1.Push(Top());
+      Pop();
+    }
+
+    while (!another_stack.empty()) {
+      extra_stack_2.Push(another_stack.top());
+      another_stack.pop();
+    }
+
+    while (!extra_stack_1.Empty()) {
+      another_stack.push(extra_stack_1.Top());
+      extra_stack_1.Pop();
+    }
+
+    while (!extra_stack_2.Empty()) {
+      Push(extra_stack_2.Top());
+      extra_stack_2.Pop();
+    }
+  }
+
+  /**
+   * @return T: значение минимума стека
+   * @throw std::logic_error: если минимум не проинициализирован
+   */
+  T GetMin() {
+    if (!is_min_init) throw std::logic_error("Minimum is not initialized");
+
+    return min_;
+  }
+
+ private:
+  /**
+   * @return T: новый минимум
+   * (после удаления)
+   */
+  void FindNewMin() {
+    min_ = top_->data;
+
+    auto iter = top_;
+    while (iter->prev != nullptr) {
+      if (iter->data < min_) min_ = iter->data;
+      iter = iter->prev;
+    }
+  }
+
+  // @brief указатель на последний элемент
+  std::shared_ptr<SinglyListElem<T>> top_;
+  size_t size_{0};
+
+  T min_;
+  bool is_min_init{false};
+};
---
+++ task_02/src/stack/singly_list_stack_test.cpp
@@ -0,0 +1,237 @@
+// std libs:
+#include <deque>
+#include <initializer_list>
+#include <list>
+#include <stack>
+#include <stdexcept>
+#include <string>
+
+// test lib:
+#include <gtest/gtest.h>
+
+// my lib:
+#include "singly_list_stack.hpp"
+
+using namespace std;
+
+TEST(SinglyListStack_test, Simple) {
+  SinglyListStack<int> stack;
+  // в стандартной реализации у стека pop() имеет void
+  stack.Push(1);              // SinglyListStack [1]
+  ASSERT_EQ(stack.Top(), 1);  // SinglyListStack [1]
+  stack.Pop();                // SinglyListStack []
+  stack.Push(1);              // SinglyListStack [1]
+  stack.Push(2);              // SinglyListStack [1, 2]
+  ASSERT_EQ(stack.Top(), 2);  // SinglyListStack [1, 2]
+  stack.Pop();                // SinglyListStack [1]
+  ASSERT_EQ(stack.Top(), 1);  // SinglyListStack [1]
+  stack.Pop();                // SinglyListStack []
+  stack.Push(1);              // SinglyListStack [1]
+  stack.Push(2);              // SinglyListStack [1, 2]
+  ASSERT_EQ(stack.Top(), 2);  // SinglyListStack [1, 2]
+  stack.Pop();                // SinglyListStack [1]
+  stack.Push(3);              // SinglyListStack [1, 3]
+  ASSERT_EQ(stack.Top(), 3);  // SinglyListStack [1, 3]
+  stack.Pop();                // SinglyListStack [1]
+  ASSERT_EQ(stack.Top(), 1);  // SinglyListStack [1]
+  stack.Pop();
+}
+
+TEST(SinglyListStack_test, Size_test) {
+  auto stack = SinglyListStack<float>();
+  ASSERT_EQ(stack.Size(), 0);
+}
+
+TEST(SinglyListStack_test, PushTop_test) {
+  auto stack = SinglyListStack<int>();
+  stack.Push(6);
+  ASSERT_EQ(stack.Top(), 6);
+}
+
+TEST(SinglyListStack_test, PushTopPop_test_1) {
+  auto stack = SinglyListStack<string>();
+  stack.Push();
+  stack.Push();
+  stack.Pop();
+  stack.Pop();
+  EXPECT_THROW(stack.Top(), logic_error);
+}
+
+TEST(SinglyListStack_test, PushTopPop_test_2) {
+  auto stack = SinglyListStack<string>();
+  stack.Push("I want to write a random text, but");
+  stack.Push("since I have to check for the correctness of words, I need");
+  stack.Push(
+      "to make sure there are no errors in it, so I am writing this strange "
+      "sentence through a translator");
+  stack.Push("the translators are very cool, you knew about this,");
+  stack.Push(
+      "I assume that they have been using neural networks for a long time");
+  ASSERT_EQ(
+      stack.Top(),
+      "I assume that they have been using neural networks for a long time");
+  stack.Pop();
+  stack.Pop();
+  stack.Pop();
+  ASSERT_EQ(stack.Top(),
+            "since I have to check for the correctness of words, I need");
+}
+
+TEST(SinglyListStack_test, PushSize_test_1) {
+  auto stack = SinglyListStack<int>();
+  stack.Push();
+  stack.Push();
+  ASSERT_EQ(stack.Size(), 2);
+}
+
+TEST(SinglyListStack_test, PushSize_test_2) {
+  auto stack = SinglyListStack<double>();
+  stack.Push();
+  stack.Push();
+  stack.Push();
+  stack.Push();
+  stack.Push();
+  ASSERT_EQ(stack.Size(), 5);
+}
+
+TEST(SinglyListStack_test, PushSize_test_3) {
+  struct A {
+    double a;
+    string s;
+    vector<A> b;
+    list<vector<deque<A>>> c;
+    A() = default;
+  };
+
+  auto stack = SinglyListStack<A>();
+  stack.Push();
+  stack.Push();
+  stack.Push();
+  stack.Push();
+  stack.Push();
+  ASSERT_EQ(stack.Size(), 5);
+}
+
+TEST(SinglyListStack_test, PushPop_test_1) {
+  auto stack = SinglyListStack<double>();
+  stack.Push();
+  stack.Push();
+  stack.Push();
+  stack.Pop();
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 1);
+}
+
+TEST(SinglyListStack_test, PushPop_test_2) {
+  auto stack = SinglyListStack<double>();
+  stack.Push();
+  stack.Push();
+  stack.Pop();
+  stack.Pop();
+  EXPECT_THROW(stack.Pop(), logic_error);
+}
+
+TEST(SinglyListStack_test, PushRange_initializer_list_SizePopTop_test) {
+  auto stack = SinglyListStack<int>();
+  stack.PushRange({4, 3, 2, 1});
+  ASSERT_EQ(stack.Size(), 4);
+  ASSERT_EQ(stack.Top(), 1);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 3);
+  ASSERT_EQ(stack.Top(), 2);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 2);
+  ASSERT_EQ(stack.Top(), 3);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 1);
+  ASSERT_EQ(stack.Top(), 4);
+  stack.Pop();
+  EXPECT_THROW(stack.Pop(), logic_error);
+  EXPECT_THROW(stack.Top(), logic_error);
+}
+
+TEST(SinglyListStack_test, PushRange_vector_SizePopTop_test_1) {
+  auto stack = SinglyListStack<int>();
+  stack.PushRange(std::vector(4, 0));
+  ASSERT_EQ(stack.Size(), 4);
+  ASSERT_EQ(stack.Top(), 0);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 3);
+  ASSERT_EQ(stack.Top(), 0);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 2);
+  ASSERT_EQ(stack.Top(), 0);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 1);
+  ASSERT_EQ(stack.Top(), 0);
+  stack.Pop();
+  EXPECT_THROW(stack.Pop(), logic_error);
+  EXPECT_THROW(stack.Top(), logic_error);
+}
+
+TEST(SinglyListStack_test, PushRange_vector_SizePopTop_test_2) {
+  auto stack = SinglyListStack<int>();
+  stack.PushRange(std::vector({4, 3, 2, 1}));
+  ASSERT_EQ(stack.Size(), 4);
+  ASSERT_EQ(stack.Top(), 1);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 3);
+  ASSERT_EQ(stack.Top(), 2);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 2);
+  ASSERT_EQ(stack.Top(), 3);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 1);
+  ASSERT_EQ(stack.Top(), 4);
+  stack.Pop();
+  EXPECT_THROW(stack.Pop(), logic_error);
+  EXPECT_THROW(stack.Top(), logic_error);
+}
+
+TEST(SinglyListStack_test, PushRange_std_stack_SizePopTop_test) {
+  auto stack = SinglyListStack<int>();
+  std::stack<int> std_stack;
+  std_stack.push(4);
+  std_stack.push(3);
+  std_stack.push(2);
+  std_stack.push(1);
+  stack.PushRange(std_stack);
+  ASSERT_EQ(stack.Size(), 4);
+  ASSERT_EQ(stack.Top(), 1);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 3);
+  ASSERT_EQ(stack.Top(), 2);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 2);
+  ASSERT_EQ(stack.Top(), 3);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 1);
+  ASSERT_EQ(stack.Top(), 4);
+  stack.Pop();
+  EXPECT_THROW(stack.Pop(), logic_error);
+  EXPECT_THROW(stack.Top(), logic_error);
+}
+
+TEST(SinglyListStack_test, PushRange_Stack_SizePopTop_test) {
+  auto stack = SinglyListStack<int>();
+  SinglyListStack<int> my_stack;
+  my_stack.Push(4);
+  my_stack.Push(3);
+  my_stack.Push(2);
+  my_stack.Push(1);
+  stack.PushRange(my_stack);
+  ASSERT_EQ(stack.Size(), 4);
+  ASSERT_EQ(stack.Top(), 1);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 3);
+  ASSERT_EQ(stack.Top(), 2);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 2);
+  ASSERT_EQ(stack.Top(), 3);
+  stack.Pop();
+  ASSERT_EQ(stack.Size(), 1);
+  ASSERT_EQ(stack.Top(), 4);
+  stack.Pop();
+  EXPECT_THROW(stack.Pop(), logic_error);
+  EXPECT_THROW(stack.Top(), logic_error);
+}
