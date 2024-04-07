1f3fa5dfafd60f2a40a8494c9fb1c455eea6c9f1
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-07 16:54:02 +0000

    add class Temperature

--- task_03/src/temperatures.hpp
+++ task_03/src/temperatures.hpp
@@ -1,1 +1,62 @@
 #pragma once
+
+// std libs:
+#include <iostream>
+#include <stdexcept>
+
+class Temperature {
+ public:
+  Temperature(float value) : value_{value} {
+    if (value < -273.15)
+      throw std::logic_error("temperature cannot be less than absolute zero");
+  }
+
+  Temperature(const Temperature& p) = default;
+
+  Temperature& operator=(const Temperature& p) = default;
+
+  Temperature& operator=(float p) {
+    *this = Temperature(p);
+    return *this;
+  }
+
+  friend Temperature& operator+=(Temperature& a, const Temperature& b);
+  friend Temperature& operator-=(Temperature& a, const Temperature& b);
+
+  auto operator<=>(const Temperature& b) const = default;
+  bool operator<(float b) const { return value_ < b; }
+  bool operator==(float b) const { return value_ == b; }
+  bool operator>(float b) const { return value_ > b; }
+  bool operator!=(float b) const { return value_ != b; }
+
+  friend std::ostream& operator<<(std::ostream& os, const Temperature& par);
+  friend std::istream& operator>>(std::istream& is, Temperature& vec);
+
+  explicit operator float() const { return value_; }
+
+ private:
+  // температура не должна быть слишком большой
+  // (поэтому здесь float, а не double)
+  float value_{0};
+};
+
+inline Temperature& operator+=(Temperature& a, const Temperature& b) {
+  a.value_ += b.value_;
+  return a;
+}
+
+inline Temperature& operator-=(Temperature& a, const Temperature& b) {
+  a.value_ -= b.value_;
+  return a;
+}
+
+inline Temperature operator+(Temperature a, Temperature b) { return a += b; }
+inline Temperature operator-(Temperature a, Temperature b) { return a -= b; }
+
+inline std::ostream& operator<<(std::ostream& os, const Temperature& t) {
+  return os << t.value_;
+}
+
+inline std::istream& operator>>(std::istream& is, Temperature& t) {
+  return is >> t.value_;
+}
