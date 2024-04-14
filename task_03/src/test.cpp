72fd7ae465cc236954505cdd27bc159f05b7f952
sh1buya <92878896+sh1buya@users.noreply.github.com>
2024-04-14 16:52:46 +0000

    task_03 done

--- task_03/src/test.cpp
+++ task_03/src/test.cpp
@@ -3,6 +3,24 @@
 
 #include "topology_sort.hpp"
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+TEST(DaysBeforeWarming, Simple) {
+  std::vector<int> temperatures = {1, 2, 3, 4, 5};
+  std::vector<int> correct_answer = {1, 1, 1, 1, 0};
+  ASSERT_EQ(DaysBeforeWarming(temperatures), correct_answer);
+
+  temperatures = {1};
+  correct_answer = {0};
+  ASSERT_EQ(DaysBeforeWarming(temperatures), correct_answer);
+
+  temperatures = {0, 0, 0, 0, 0, 5};
+  correct_answer = {5, 4, 3, 2, 1, 0};
+  ASSERT_EQ(DaysBeforeWarming(temperatures), correct_answer);
+
+  temperatures = {0, 0, 0, 0, 0, 0};
+  correct_answer = {0, 0, 0, 0, 0, 0};
+  ASSERT_EQ(DaysBeforeWarming(temperatures), correct_answer);
+
+  temperatures = {3, 12, 4, 4, 6, 2, 1, 9, 2};
+  correct_answer = {1, 0, 2, 1, 3, 2, 1, 0, 0};
+  ASSERT_EQ(DaysBeforeWarming(temperatures), correct_answer);
 }
--- task_03/src/topology_sort.cpp
+++ task_03/src/topology_sort.cpp
@@ -1,1 +1,27 @@
 #include "topology_sort.hpp"
+
+std::vector<int> DaysBeforeWarming(std::vector<int> temperatures) {
+  std::vector<int> answer(temperatures.size());
+  for (int i = 0; i < temperatures.size(); i++) {
+    answer[i] = 1;
+    bool was_warmer_found = false;
+    for (int j = i + 1; j < temperatures.size() and not was_warmer_found; j++) {
+      if (temperatures[i] >=
+          temperatures[j]) {  // skip days, when tempreture is lower
+        answer[i] += 1;
+      } else if ((temperatures[i] < temperatures[j]) &
+                 (j == i + 1)) {  // case, when warmer day is rigth next
+        answer[i] = 1;
+        was_warmer_found = true;
+      } else if ((temperatures[i] < temperatures[j]) &
+                 (j != i + 1)) {  // case, when warmer day is rigth next
+        was_warmer_found = true;
+      }
+    }
+
+    if (not was_warmer_found) {
+      answer[i] = 0;
+    }
+  }
+  return answer;
+}
