b2700c8a510de2d12f77da7e1c8980d5c9999932
sh1buya <92878896+sh1buya@users.noreply.github.com>
2024-04-14 20:32:50 +0000

    Heap and heap_sort with undefined reference errors

--- task_04/src/test.cpp
+++ task_04/src/test.cpp
@@ -1,6 +1,32 @@
 
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+#include <vector>
+
+#include "heap.hpp"
+
+TEST(HeapTest, Simple) {
+  Heap heap;
+  std::vector<int> answer = {};
+  heap.pop_min();
+  ASSERT_EQ(heap.get_data(), answer);  // pop from empty heap
+
+  heap.insert(1);
+  heap.insert(2);
+  heap.insert(3);
+  heap.insert(4);
+  heap.insert(5);
+  heap.insert(6);
+
+  heap.insert(0);
+  answer = {0, 2, 1, 4, 5, 6, 3};
+  ASSERT_EQ(heap.get_data(), answer);  // insert key, smaller than its parent
+
+  heap.pop_min();
+  answer = {1, 2, 3, 4, 5, 6};
+  ASSERT_EQ(heap.get_data(), answer);  // pop from full heap
+
+  heap.insert(100);
+  answer = {1, 2, 3, 4, 5, 6, 100};
+  ASSERT_EQ(heap.get_data(), answer);  // insert key, smaller than its parent
 }
