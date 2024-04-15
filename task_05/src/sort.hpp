0a81053e38d0b2706ec6725c35d8109edceb22a6
H0p1ty <142249611+H0p1ty@users.noreply.github.com>
2024-04-15 15:05:24 +0000

    task_05 done

--- task_05/src/sort.hpp
+++ task_05/src/sort.hpp
@@ -1,5 +1,8 @@
+#pragma once
 #include <concepts>
 #include <functional>
+#include <iostream>
+#include <stdexcept>
 
 // try to deduce if type is comparable (naive solution)
 template <typename T>
@@ -14,22 +17,33 @@ constexpr bool is_comparable() {
 }
 
 template <typename T>
-requires(is_comparable<T>()) void sort_two_sorted_arrays(T* begin1, T* begin2,
-                                                         T* end) {
-  T array[(end - begin1)];
+requires(is_comparable<T>()) void merge_two_sorted_arrays(T* begin1, T* begin2,
+                                                          T* end) {
+  if (begin1 == end) throw std::runtime_error("merge two empty arrays\n");
+
+  size_t size = end - begin1;
+  T array[size];
   size_t index = 0;
-  while (begin1 < begin2 || begin2 < end)
-    if (begin1 < begin2 && begin2 < end)
+
+  while ((begin1 < begin2) || (begin2 < end)) {
+    if ((begin1 < begin2) && (begin2 < end))
       (*begin1 <= *begin2) ? array[index++] = *(begin1++)   // this line
                            : array[index++] = *(begin2++);  // is cursed
-    else
-      (begin1 < begin2 && begin2 == end)
-          ? array[index++] = *(begin1++)   // this line
-          : array[index++] = *(begin2++);  // as well
+    else if ((begin1 < begin2) && (begin2 == end))
+      array[index++] = *(begin1++);
+    else if ((begin1 == begin2) && (begin2 < end))
+      array[index++] = *(begin1++);
+  }
+
   index = 0;
   for (T* iterator = begin1; iterator < end; iterator++)
     *iterator = array[index++];
 }
 
 template <typename T>
-requires(is_comparable<T>()) void merge_sort(const T* begin, const T* end) {}
+requires(is_comparable<T>()) void merge_sort(T* begin, T* end) {
+  if ((end == begin) || (end - begin == 1)) return;
+  merge_sort(begin, begin + (end - begin) / 2);
+  merge_sort(begin + (end - begin) / 2, end);
+  merge_two_sorted_arrays(begin, begin + (end - begin) / 2, end);
+}
--- task_05/src/test.cpp
+++ task_05/src/test.cpp
@@ -1,6 +1,67 @@
-
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+#include <string>
+#include <vector>
+
+#include "sort.hpp"
+
+using std::string;
+
+TEST(sort, empty) {
+  std::vector<int> vector1 = {};
+  std::vector<int> assert_vector1 = {};
+  int* begin = &vector1[0];
+  int* end = &vector1[vector1.size()];
+  merge_sort(begin, end);
+  ASSERT_EQ(vector1, assert_vector1);
 }
+TEST(sort, one_elem) {
+  std::vector<int> vector2 = {1};
+  std::vector<int> assert_vector2 = {1};
+  int* begin = &vector2[0];
+  int* end = &vector2[vector2.size()];
+  merge_sort(begin, end);
+  ASSERT_EQ(vector2, assert_vector2);
+}
+
+TEST(sort, two_elem) {
+  std::vector<int> vector3 = {1, 2};
+  std::vector<int> assert_vector3 = {1, 2};
+  int* begin = &vector3[0];
+  int* end = &vector3[vector3.size()];
+  merge_sort(begin, end);
+  ASSERT_EQ(vector3, assert_vector3);
+
+  std::vector<int> vector4 = {2, 1};
+  std::vector<int> assert_vector4 = {1, 2};
+  begin = &vector4[0];
+  end = &vector4[vector4.size()];
+  merge_sort(begin, end);
+  ASSERT_EQ(vector4, assert_vector4);
+}
+
+TEST(sort, multiple_elem) {
+  std::vector<int> vector5 = {1, 3, 2, 5, 4};
+  std::vector<int> assert_vector5 = {1, 2, 3, 4, 5};
+  int* begin = &vector5[0];
+  int* end = &vector5[vector5.size()];
+  merge_sort(begin, end);
+  ASSERT_EQ(vector5, assert_vector5);
+}
+
+// Test below fails to check at compile time bc of use of concepts
+//
+//                              |
+//                              |
+//                              V
+
+/*
+TEST(sort, strings) {
+  std::vector<string> vector6 = {"bb", "ba", "ab", "aa"};
+  std::vector<string> assert_vector6 = {"aa", "ab", "ba", "bb"};
+  string* begin = &vector6[0];
+  string* end = &vector6[vector6.size()];
+  merge_sort(begin, end);
+  ASSERT_EQ(vector6, assert_vector6);
+}
+*/
