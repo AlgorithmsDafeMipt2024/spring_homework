39efd9c9b5a3f5fc51dad0c317eeb49d68a19e71
Ilya_Rybalkin <rybalkin.id@phystech.edu>
2024-04-15 20:48:51 +0000

    task_02

--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -1,42 +1,54 @@
 
 #include <gtest/gtest.h>
 
-#include <stack>
-
 #include "stack.hpp"
 
 TEST(StackTest, Simple) {
-  Stack stack;
-  stack.Push(1);              // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  stack.Push(3);              // Stack [1, 3]
-  ASSERT_EQ(stack.Pop(), 3);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
+  Stack<int> stack;
+  stack.Push(1);         
+  ASSERT_EQ(stack.Pop(), 1);
 }
 
 TEST(MinStackTest, Simple) {
-  MinStack stack;
-  stack.Push(1);  // Stack [1]
+  MinStack<int> stack;
+  stack.Push(3);
+  stack.Push(1);
   ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
+  ASSERT_EQ(stack.Pop(), 1);
+}
+
+TEST(StackTest, Complex) {
+  Stack<int> stack;
+  stack.Push(1);
+  ASSERT_EQ(stack.Pop(), 1);
+  stack.Push(1);
+  stack.Push(2);
+  ASSERT_EQ(stack.Pop(), 2);
+  ASSERT_EQ(stack.Pop(), 1);
+  stack.Push(1);
+  stack.Push(2);
+  ASSERT_EQ(stack.Pop(), 2);
+  stack.Push(3);
+  ASSERT_EQ(stack.Pop(), 3);
+  ASSERT_EQ(stack.Pop(), 1);
+}
+
+TEST(MinStackTest, Complex) {
+  MinStack<int> stack;
+  stack.Push(1);
   ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
+  ASSERT_EQ(stack.Pop(), 1);
+  stack.Push(1);
+  stack.Push(2);
   ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  stack.Push(3);              // Stack [1, 3]
+  ASSERT_EQ(stack.Pop(), 2);
+  ASSERT_EQ(stack.Pop(), 1);
+  stack.Push(1);
+  stack.Push(2);
   ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 3);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
+  ASSERT_EQ(stack.Pop(), 2);
+  stack.Push(3);
+  ASSERT_EQ(stack.GetMin(), 1);
+  ASSERT_EQ(stack.Pop(), 3);
+  ASSERT_EQ(stack.Pop(), 1);
 }
