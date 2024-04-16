c7dd0a5f4b0e2625006d282d5ced592dffef6003
Ilya_Rybalkin <rybalkin.id@phystech.edu>
2024-04-16 12:51:32 +0000

    task_02

--- task_02/src/stack.cpp
+++ task_02/src/stack.cpp
@@ -1,21 +1,49 @@
 #include "stack.hpp"
 
-#include <algorithm>
+template <typename T> size_t Stack<T>::Size() const { return size_; }
 
-void Stack::Push(int value) { data_.push(value); }
-
-int Stack::Pop() {
-  auto result = data_.top();
-  data_.pop();
-  return result;
+template <typename T> void Stack<T>::Push(T data)
+{
+  top_ = std::make_shared<Element>(data, top_);
+  size_++;
 }
 
-void MinStack::Push(int value) { data_.push_back(value); }
-
-int MinStack::Pop() {
-  auto result = data_.back();
-  data_.pop_back();
-  return result;
+template <typename T> T Stack<T>::Pop()
+{
+  if (size_ == 0)
+    throw std::runtime_error("Stack is empty");
+  T removed_value = top_->data_;
+  top_ = std::move(top_->next_);
+  size_--;
+  return removed_value;
 }
 
-int MinStack::GetMin() { return *std::min_element(data_.begin(), data_.end()); }
+template <typename T> T Stack<T>::Top() const
+{
+  if (size_ == 0)
+    throw std::runtime_error("Stack is empty");
+  return top_->data_;
+}
+
+template <typename T> void MinStack<T>::Push(T data)
+{
+  top_ = std::make_shared<Element>(data, top_);
+  size_++;
+  if (minimums_stack_.Size() > 0 && data > minimums_stack_.Top())
+    minimums_stack_.Push(minimums_stack_.Top());
+  else
+    minimums_stack_.Push(data);
+}
+
+template <typename T> T MinStack<T>::Pop()
+{
+  if (size_ == 0)
+    throw std::runtime_error("Stack is empty");
+  T removed_value = top_->data_;
+  top_ = std::move(top_->next_);
+  size_--;
+  minimums_stack_.Pop();
+  return removed_value;
+}
+
+template <typename T> T MinStack<T>::GetMin() const { return minimums_stack_.Top(); }
--- task_02/src/stack.hpp
+++ task_02/src/stack.hpp
@@ -1,23 +1,46 @@
 #pragma once
 
-#include <stack>
-#include <vector>
+#include <memory>
 
-class Stack {
- public:
-  void Push(int value);
-  int Pop();
+/// @brief Элемент стека
+template <typename T> struct Element
+{
+  explicit Element(T data, const std::shared_ptr<Element>& next) : data_(data), next_(next) {}
 
- private:
-  std::stack<int> data_;
+  T data_;
+  std::shared_ptr<Element> next_;
 };
 
-class MinStack {
- public:
-  void Push(int value);
-  int Pop();
-  int GetMin();
+/// @brief Cтек
+/// @tparam Т Тип данных
+template <typename T> class Stack
+{
+public:
+  explicit Stack() : top_(nullptr), size_{0} {}
 
- private:
-  std::vector<int> data_;
+  size_t Size () const;
+  virtual void Push (T data);
+  virtual T Pop ();
+  virtual T Top () const;
+
+private:
+  size_t size_;
+  std::shared_ptr<Element<T>> top_;
+};
+
+/// @brief Стек с минимумом
+/// @tparam T Тип данных
+template <typename T> class MinStack : public Stack<T>
+{
+public:
+  explicit MinStack() : Stack<T>(), minimums_stack_() {}
+
+  void Push (T data) override;
+  T Pop () override;
+  T GetMin () const;
+
+private:
+  Stack<T> minimums_stack_;
+  size_t size_;
+  std::shared_ptr<Element<T>> top_;
 };
