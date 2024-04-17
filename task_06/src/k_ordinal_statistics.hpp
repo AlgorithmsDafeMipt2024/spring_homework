6c642c0b7b6aca8d10665044325c0379fd96c0f2
H0p1ty <142249611+H0p1ty@users.noreply.github.com>
2024-04-17 20:42:24 +0000

    task_06 done

---
+++ task_06/src/k_ordinal_statistics.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <cstddef>
+#include <stdexcept>
+#include <vector>
+
+template <typename CustomType>
+concept comparable = requires(CustomType a, CustomType b) {
+  a < b;
+  a <= b;
+  a == b;
+  a >= b;
+  a > b;
+  a != b;
+};
+
+template <comparable CustomType>
+size_t partition(std::vector<CustomType>& data_array, size_t left_index,
+                 size_t right_index) {
+  CustomType& separator = data_array[(left_index + right_index) / 2];
+  size_t left = left_index;
+  size_t right = right_index;
+  while (left <= right) {
+    while (data_array[left] < separator) left++;
+    while (data_array[right] > separator) right--;
+    if (left >= right) break;
+    std::swap(data_array[left++], data_array[right--]);
+  }
+  return right;
+}
+
+template <comparable CustomType>
+CustomType find_ordinal_statistic(std::vector<CustomType> data_array,
+                                  size_t k) {
+  if (k >= data_array.size())
+    throw std::runtime_error("k is greater than the size of the array\n");
+  size_t left = 0;
+  size_t right = data_array.size() - 1;
+  for (;;) {
+    size_t middle = partition(data_array, left, right);
+
+    if (middle == k)
+      return data_array[middle];
+    else if (k < middle)
+      right = middle;
+    else
+      left = middle + 1;
+  }
+}
--- task_06/src/test.cpp
+++ task_06/src/test.cpp
@@ -1,6 +1,50 @@
 
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+#include <stdexcept>
+#include <string>
+
+#include "k_ordinal_statistics.hpp"
+
+using std::string;
+
+TEST(k_ordinal_statistics, wrong_data) {
+  std::vector<int> vector1 = {};
+  EXPECT_THROW(find_ordinal_statistic(vector1, 0), std::runtime_error);
+}
+
+TEST(k_ordinal_statistics, already_sorted) {
+  std::vector<int> vector2 = {1, 2, 3, 4, 5, 6};
+  int answer2_1 = 1;
+  int answer2_2 = 4;
+  int answer2_3 = 6;
+  ASSERT_EQ(answer2_1, find_ordinal_statistic(vector2, 0));
+  ASSERT_EQ(answer2_2, find_ordinal_statistic(vector2, 3));
+  ASSERT_EQ(answer2_3, find_ordinal_statistic(vector2, 5));
+}
+
+TEST(k_ordinal_statistics, unsorted_1) {
+  std::vector<int> vector3 = {10, 5, 6, 3, 4, 1, 2, 7, 8, 9};
+  int answer3_1 = 5;
+  int answer3_2 = 10;
+  int answer3_3 = 1;
+  ASSERT_EQ(answer3_1, find_ordinal_statistic(vector3, 4));
+  ASSERT_EQ(answer3_2, find_ordinal_statistic(vector3, 9));
+  ASSERT_EQ(answer3_3, find_ordinal_statistic(vector3, 0));
+}
+
+TEST(k_ordinal_statistics, unsorted_2) {
+  std::vector<int> vector4 = {111, 201, 50, -10, -405};
+  int answer4_1 = 50;
+  int answer4_2 = -405;
+  ASSERT_EQ(answer4_1, find_ordinal_statistic(vector4, 2));
+  ASSERT_EQ(answer4_2, find_ordinal_statistic(vector4, 0));
+}
+
+TEST(k_ordinal_statistics, string_array) {
+  std::vector<string> vector5 = {"bbb", "bab", "aaa", "caa", "cab"};
+  string answer5_1 = "aaa";
+  string answer5_2 = "bbb";
+  ASSERT_EQ(answer5_1, find_ordinal_statistic(vector5, 0));
+  ASSERT_EQ(answer5_2, find_ordinal_statistic(vector5, 2));
 }
