7bd6195f0a335c1319ba041381e335f5eced9304
Romanov-Fedor <143000900+Romanov-Fedor@users.noreply.github.com>
2024-04-17 08:25:12 +0000

    task_02 complete

--- task_02/src/stack.cpp
+++ task_02/src/stack.cpp
@@ -1,21 +1,46 @@
 #include "stack.hpp"
 
-#include <algorithm>
+#include <memory>
 
-void Stack::Push(int value) { data_.push(value); }
+void Stack::Push(int value) {
+  Element new_data{value, std::make_shared<Element>(data_)};
+  size_ += 1;
+  data_ = new_data;
+}
 
 int Stack::Pop() {
-  auto result = data_.top();
-  data_.pop();
-  return result;
+  int deleted_element = data_.element;
+  Element new_data = *data_.prev;
+  data_.prev = nullptr;
+  data_ = new_data;
+  size_ -= 1;
+  return deleted_element;
 }
 
-void MinStack::Push(int value) { data_.push_back(value); }
+bool Stack::Empty() const { return data_.prev == nullptr; }
+
+const int& Stack::Top() const { return data_.element; }
+
+size_t Stack::Size() const { return size_; }
+
+void MinStack::Push(int value) {
+  if (main_stack_.Size() == 0) {
+    min_stack_.Push(value);
+    main_stack_.Push(value);
+  } else {
+    main_stack_.Push(value);
+    if (value < min_stack_.Top())
+      min_stack_.Push(value);
+    else
+      min_stack_.Push(min_stack_.Top());
+  }
+}
 
 int MinStack::Pop() {
-  auto result = data_.back();
-  data_.pop_back();
+  int result = main_stack_.Top();
+  main_stack_.Pop();
+  min_stack_.Pop();
   return result;
 }
 
-int MinStack::GetMin() { return *std::min_element(data_.begin(), data_.end()); }
+int MinStack::GetMin() const { return min_stack_.Top(); }
--- task_02/src/stack.hpp
+++ task_02/src/stack.hpp
@@ -1,23 +1,45 @@
 #pragma once
 
-#include <stack>
-#include <vector>
+#include <memory>
+
+struct Element {
+  explicit Element(int data, std::shared_ptr<Element> prev = nullptr)
+      : element{data}, prev{prev} {}
+
+  explicit Element() : element{}, prev{nullptr} {}
+
+  int element;
+
+  std::shared_ptr<Element> prev;
+};
 
 class Stack {
  public:
+  explicit Stack() : data_{} {}
+
   void Push(int value);
+
   int Pop();
 
+  bool Empty() const;
+
+  const int& Top() const;
+
+  size_t Size() const;
+
  private:
-  std::stack<int> data_;
+  Element data_;
+
+  size_t size_ = 0;
 };
 
 class MinStack {
  public:
   void Push(int value);
   int Pop();
-  int GetMin();
+  int GetMin() const;
 
  private:
-  std::vector<int> data_;
+  Stack main_stack_;
+  Stack min_stack_;
 };
