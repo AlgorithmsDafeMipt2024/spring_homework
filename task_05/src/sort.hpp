3949b624b68188ddf903c62e01a9e9ef0fb785f2
H0p1ty <142249611+H0p1ty@users.noreply.github.com>
2024-04-17 16:02:39 +0000

    task_05 perfected

--- task_05/src/sort.hpp
+++ task_05/src/sort.hpp
@@ -6,8 +6,9 @@
 #include <stdexcept>
 #include <utility>
 
-template <typename T>
-concept comparable = requires(T a, T b) {
+// concept to check if the type is comparable (now obsolete)
+template <typename CustomType>
+concept comparable = requires(CustomType a, CustomType b) {
   a < b;
   a <= b;
   a == b;
@@ -16,20 +17,31 @@ concept comparable = requires(T a, T b) {
   a != b;
 };
 
-template <comparable T>
-void merge_two_sorted_arrays(T* begin, T* middle, T* end) {
+// concept to check if the type is comparable with a concrete function
+template <typename Function, typename CustomType>
+concept comparing = requires(CustomType a, CustomType b, Function f) {
+  f(a, b);
+};
+
+template <typename CustomType, comparing<CustomType> Function =
+                                   std::function<bool(CustomType, CustomType)>>
+void merge_two_sorted_arrays(
+    CustomType* begin, CustomType* middle, CustomType* end,
+    Function comparing_function = [](CustomType a, CustomType b) {
+      return a < b;
+    }) {
   if (begin == end) throw std::runtime_error("merge two empty arrays\n");
 
   size_t size = end - begin;
-  T array[size];
+  CustomType array[size];
   size_t index = 0;
 
-  T* begin_reserve = begin;
-  T* middle_reserve = middle;
+  CustomType* begin_reserve = begin;
+  CustomType* middle_reserve = middle;
 
   while ((begin < middle_reserve) || (middle < end)) {
     if ((begin < middle_reserve) && (middle < end))
-      (*begin < *middle)
+      (comparing_function(*begin, *middle))
           ? array[index++] = std::move(*(begin++))    // this line
           : array[index++] = std::move(*(middle++));  // is cursed
 
@@ -42,18 +54,25 @@ void merge_two_sorted_arrays(T* begin, T* middle, T* end) {
   }
 
   index = 0;
-  for (T* iterator = begin_reserve; iterator < end; iterator++) {
+  for (CustomType* iterator = begin_reserve; iterator < end; iterator++) {
     *iterator = std::move(array[index]);
     index++;
   }
 }
 
-template <comparable T>
-void merge_sort(T* begin, T* end) {
+template <typename CustomType, comparing<CustomType> Function =
+                                   std::function<bool(CustomType, CustomType)>>
+void merge_sort(
+    CustomType* begin, CustomType* end,
+    Function comparing_function = [](CustomType a, CustomType b) {
+      return a < b;
+    }) {
   if ((end == begin) || (end - begin == 1)) return;
 
-  T* middle_begin = begin + (end - begin) / 2;
-  merge_sort(begin, middle_begin);
-  merge_sort(middle_begin, end);
-  merge_two_sorted_arrays(begin, middle_begin, end);
+  CustomType* middle_begin = begin + (end - begin) / 2;
+  merge_sort(begin, middle_begin, comparing_function);
+  merge_sort(middle_begin, end, comparing_function);
+  merge_two_sorted_arrays(begin, middle_begin, end, comparing_function);
 }
+
+// this took way too long
--- task_05/src/test.cpp
+++ task_05/src/test.cpp
@@ -70,8 +70,37 @@ TEST(sort, strings) {
   std::sort(assert_vector7.begin(), assert_vector7.end());
   string* begin = &vector7[0];
   string* end = &vector7[vector7.size()];
-
   merge_sort(begin, end);
+  ASSERT_EQ(vector7, assert_vector7);
+}
 
-  ASSERT_EQ(vector7 == assert_vector7, true);
+TEST(reverse_sort, multiple_elem) {
+  std::vector<int> vector8 = {1, 2, 3, 4, 5};
+  std::vector<int> assert_vector8 = vector8;
+  std::sort(assert_vector8.begin(), assert_vector8.end(),
+            [](int a, int b) { return a >= b; });
+  int* begin = &vector8[0];
+  int* end = &vector8[vector8.size()];
+  merge_sort(begin, end, [](int a, int b) { return a >= b; });
+  ASSERT_EQ(vector8, assert_vector8);
+}
+
+bool sum_comparasion(std::vector<int> a, std::vector<int> b) {
+  int sum1 = 0;
+  for (size_t ind = 0; ind < a.size(); ind++) sum1 += a[ind];
+  int sum2 = 0;
+  for (size_t ind = 0; ind < b.size(); ind++) sum2 += b[ind];
+  return sum1 < sum2;
+}
+
+TEST(sum_sort, multiple_elem) {
+  std::vector<std::vector<int>> vector9 = {{5, 1, 1}, {4, 4, 4}, {1, 1, 0}};
+  std::vector<std::vector<int>> assert_vector9 = vector9;
+  std::sort(assert_vector9.begin(), assert_vector9.end());
+  std::vector<int>* begin = &vector9[0];
+  std::vector<int>* end = &vector9[vector9.size()];
+  merge_sort(begin, end, sum_comparasion);
+  ASSERT_EQ(vector9 == assert_vector9, false);
+  std::sort(assert_vector9.begin(), assert_vector9.end(), sum_comparasion);
+  ASSERT_EQ(vector9 == assert_vector9, true);
 }
