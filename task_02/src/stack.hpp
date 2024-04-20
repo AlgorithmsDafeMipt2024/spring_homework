97f3b43520964085a0a6c49b6c1604221238817e
H0p1ty <142249611+H0p1ty@users.noreply.github.com>
2024-04-20 09:46:03 +0000

    task_02 added concepts

--- task_02/src/stack.hpp
+++ task_02/src/stack.hpp
@@ -6,6 +6,16 @@
 #include <vector>
 
 template <typename T>
+concept comparable = requires(T a, T b) {
+  a < b;
+  a <= b;
+  a == b;
+  a >= b;
+  a > b;
+  a != b;
+};
+
+template <comparable T>
 struct Node {
   Node() : prev{nullptr}, data{}, next{nullptr} {}
   Node(T data_) : prev{nullptr}, data{data_}, next{nullptr} {}
@@ -15,13 +25,13 @@ struct Node {
   T data;
 };
 
-template <typename T>
+template <comparable T>
 void Node<T>::add_node(T data_) {
   next = new Node(data_);
   next->prev = this;
 }
 
-template <typename T>
+template <comparable T>
 class MyStack {
  public:
   MyStack() : top_{nullptr} {}
@@ -35,7 +45,7 @@ class MyStack {
   Node<T>* top_;
 };
 
-template <typename T>
+template <comparable T>
 void MyStack<T>::push(T value) {
   if (top_ == nullptr)
     top_ = new Node(value);
@@ -45,14 +55,14 @@ void MyStack<T>::push(T value) {
   }
 }
 
-template <typename T>
+template <comparable T>
 T MyStack<T>::top() {
   if (top_ == nullptr)
     throw std::runtime_error("there are no elements in the stack\n");
   return top_->data;
 }
 
-template <typename T>
+template <comparable T>
 T MyStack<T>::pop() {
   if (top_ == nullptr)
     throw std::runtime_error("there are no elements in the stack\n");
@@ -64,7 +74,7 @@ T MyStack<T>::pop() {
 }
 
 // works only if your type is comparable!!!
-template <typename T>
+template <comparable T>
 class MinStack {
  public:
   void push(T value);
@@ -76,7 +86,7 @@ class MinStack {
   MyStack<std::pair<T, T>> stack;
 };
 
-template <typename T>
+template <comparable T>
 void MinStack<T>::push(T value) {
   if (stack.empty())
     stack.push({value, value});
@@ -84,17 +94,17 @@ void MinStack<T>::push(T value) {
     stack.push({value, std::min(value, stack.top().second)});
 }
 
-template <typename T>
+template <comparable T>
 T MinStack<T>::pop() {
   return stack.pop().first;
 }
 
-template <typename T>
+template <comparable T>
 T MinStack<T>::min() {
   return stack.top().second;
 }
 
-template <typename T>
+template <comparable T>
 T MinStack<T>::top() {
   return stack.top().first;
 }
