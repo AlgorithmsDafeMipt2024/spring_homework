38cf61f357b48e741ceb9bd369d17ed0fa309bdb
Matvey-cmd <143873323+Matvey-cmd@users.noreply.github.com>
2024-04-26 15:18:45 +0000

    delete task01

--- task_01/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <vector>
-
-#include "util.hpp"
-#include "utils.hpp"
-
-int main() { return 0; }
--- task_01/src/utils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-
-class WrongVector : public std::runtime_error {
-  using std::runtime_error::runtime_error;
-};
-
-std::pair<int, int> SumOfElements(int num, const std::vector<int> arr);
