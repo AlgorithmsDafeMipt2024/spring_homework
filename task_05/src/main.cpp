eb2429e1fec1edad562887e71e7609b6edeee3b0
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-29 21:13:30 +0000

    start testing

--- task_05/src/main.cpp
+++ task_05/src/main.cpp
@@ -9,20 +9,4 @@
 // my libs:
 #include <util.hpp>
 
-int main() {
-  std::vector<int> a{5, 3, 4, 1, 2};
-  HeapSort(a);
-  std::cout << a << std::endl;
-  std::cout << std::is_sorted(a.begin(), a.end()) << std::endl;
-
-  std::vector<int> b{5, 3, 4, 1, 2};
-  MergeSort(b);
-  std::cout << b << std::endl;
-  std::cout << std::is_sorted(b.begin(), b.end()) << std::endl;
-
-  std::vector<int> c{5, 3, 4, 1, 2};
-  QuickSort(c);
-  std::cout << c << std::endl;
-  std::cout << std::is_sorted(c.begin(), c.end()) << std::endl;
-  return 0;
-}
+int main() { return 0; }
--- task_05/src/test.cpp
+++ task_05/src/test.cpp
@@ -1,6 +1,24 @@
+// std libs:
+#include <iostream>
 
+// test lib:
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+// sort algos:
+#include <heap_sort.hpp>
+#include <merge_sort.hpp>
+#include <quick_sort.hpp>
+
+TEST(Sort, Simple) {
+  std::vector<int> a{5, 3, 4, 1, 2};
+  HeapSort(a);
+  ASSERT_EQ(std::is_sorted(a.begin(), a.end()), true);
+
+  std::vector<int> b{5, 3, 4, 1, 2};
+  MergeSort(b);
+  ASSERT_EQ(std::is_sorted(b.begin(), b.end()), true);
+
+  std::vector<int> c{5, 3, 4, 1, 2};
+  QuickSort(c);
+  ASSERT_EQ(std::is_sorted(c.begin(), c.end()), true);
 }
