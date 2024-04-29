2bd5f78239f46ea192f347d973f31cf4d2c7eac6
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-04-29 18:23:36 +0000

    add primary version of HeapSort (completely non-working)

---
+++ task_05/src/heap_sort.hpp
@@ -0,0 +1,116 @@
+#include <vector>
+
+/**
+ * @brief Возвращает элемент, который находится выше
+ * @param index: индекс элемента, выше которого нужное значение
+ * @return T: элемент, который находится выше
+ */
+size_t ParentOf(size_t index) { return (index - 1) / 2; }
+
+/**
+ * @brief Возвращает элемент, который находится левее
+ * @param index: индекс элемента, левее которого нужное значение
+ * @return T: элемент, который находится левее
+ */
+size_t LeftChildOf(size_t index) { return (2 * index + 1); }
+
+/**
+ * @brief Возвращает элемент, который находится правее
+ * @param index: индекс элемента, правее которого нужное значение
+ * @return T: элемент, который находится правее
+ */
+size_t RightChildOf(size_t index) { return (2 * index + 2); }
+
+/**
+ * @brief Приводит кучу в валидное состояние после добавления
+ * @tparam T: тип элементов в куче
+ * @param data: вектор, представляющий собой кучу
+ */
+template <typename T>
+void SiftUpHeap(std::vector<T>& data) {
+  // идем с конца по родителям
+  size_t index = data.size() - 1;
+
+  // пока следующий родитель больше, меняем местами
+  while (index > 0 && data[ParentOf(index)] < data[index]) {
+    std::swap(data[index], data[ParentOf(index)]);
+    index = ParentOf(index);
+  }
+}
+
+/**
+ * @brief Добавляет элемент в кучу
+ * @tparam T: тип элементов в куче
+ * @param data: вектор, представляющий собой кучу
+ * @param elem: значение элемента
+ */
+template <typename T>
+void AddToHeap(std::vector<T>& data, T elem) {
+  data.push_back(elem);
+  SiftUpHeap(data);
+}
+
+/**
+ * @brief Создаёт кучу по вектору
+ * @tparam T: тип элементов в куче
+ * @param data: изначальный вектор
+ * @return std::vector<T>: созданная куча в виде вектора
+ */
+template <typename T>
+std::vector<T> GetHeap(const std::vector<T>& data) {
+  std::vector<T> res;
+  for (const auto& elem : data) AddToHeap(res, elem);
+  return res;
+}
+
+/**
+ * @brief Приводит кучу в валидное состояние после удаления
+ * @tparam T: тип элементов в куче
+ * @param data: вектор, представляющий собой кучу
+ * @param start
+ * @param end
+ */
+template <typename T>
+void SiftDownHeap(std::vector<T>& data, size_t start, size_t end) {
+  while (start < end) {
+    size_t largest = start;  // largest = корень поддерева
+
+    // если левый потомок существует и больше текущего лучшего элемента
+    if (LeftChildOf(start) < end && data[LeftChildOf(start)] > data[largest]) {
+      largest = LeftChildOf(start);
+    }
+
+    // если правый потомок существует и больше текущего лучшего элемента
+    if (RightChildOf(start) < end &&
+        data[RightChildOf(start)] > data[largest]) {
+      largest = RightChildOf(start);
+    }
+
+    // если текущий элемент не больше своих потомков, завершаем итерацию
+    if (largest == start) break;
+
+    // меняем местами текущий элемент с наибольшим из потомков
+    std::swap(data[start], data[largest]);
+
+    // переходим вниз по дереву к потомку, с которым произвели обмен
+    start = largest;
+  }
+}
+
+/**
+ * @brief Сортировка кучей, пирамидальная сортировка (англ. Heapsort) — алгоритм
+ сортировки, использующий структуру данных двоичная куча. Это неустойчивый
+ алгоритм сортировки с временем работы O(nlogn), где n — количество элементов
+ для сортировки, и использующий O(1) дополнительной памяти.
+ * @tparam T: тип элементов в куче
+ * @param data: сортируемый вектор
+ */
+template <typename T>
+void HeapSort(std::vector<T>& data) {
+  data = GetHeap(data);
+  auto n = data.size();
+  auto index = data.size();
+  for (size_t i = 0; i < n - 1; i++) std::swap(data[0], data[n - 1 - i]);
+  index--;
+  SiftDownHeap(data, 0, index);
+}
--- task_05/src/main.cpp
+++ task_05/src/main.cpp
@@ -1,3 +1,10 @@
+#include <heap_sort.hpp>
 #include <iostream>
+#include <util.hpp>
 
-int main() { return 0; }
+int main() {
+  std::vector<int> a{3, 4, 6, 1, 8};
+  HeapSort(a);
+  std::cout << a << std::endl;
+  return 0;
+}
