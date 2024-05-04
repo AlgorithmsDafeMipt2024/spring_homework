b6da086a7cd7daa94b4b237915d711ea10fc7c8e
gulyonatyoma <143820164+gulyonatyoma@users.noreply.github.com>
2024-05-04 12:13:32 +0000

    listnode fix

--- lib/src/listnode.hpp
+++ lib/src/listnode.hpp
@@ -19,12 +19,7 @@ void ListNode<CustomClass>::AddNodeFront(CustomClass data_) {
   ListNode* next_reserved = next;
   next = new ListNode(data_);
   next->prev = this;
-  if (next_reserved != nullptr) {
-    if (next_reserved->prev != this)
-      throw std::logic_error("nodes aren't correctly connected\n");
-    next_reserved->prev = next;
-    next->next = next_reserved;
-  }
+  next->next = next_reserved;
 }
 
 template <constructable CustomClass>
@@ -32,10 +27,5 @@ void ListNode<CustomClass>::AddNodeBack(CustomClass data_) {
   ListNode* prev_reserved = prev;
   prev = new ListNode(data_);
   next->next = this;
-  if (prev_reserved != nullptr) {
-    if (prev_reserved->next != this)
-      throw std::logic_error("nodes aren't correctly connected\n");
-    prev_reserved->next = prev;
-    prev->prev = prev_reserved;
-  }
+  prev->prev = prev_reserved;
 }
