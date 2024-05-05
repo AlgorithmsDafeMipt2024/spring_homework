8f4fefdfa640f5b8aa0119a9410e944938943972
dfsavffc <144057175+dfsavffc@users.noreply.github.com>
2024-05-05 17:15:09 +0000

    some fix

--- task_01/src/topology_sort.hpp
+++ task_01/src/topology_sort.hpp
@@ -1,11 +1,1 @@
 #pragma once
-#include <stdexcept>
-#include <utility>
-#include <vector>
-
-class NoAnswer : public std::runtime_error {
- public:
-  using std::runtime_error::runtime_error;
-};
-
-std::pair<int, int> FindSummands(const std::vector<int> array, int number);
