f3b691e5db2fc1604e66af945fdde392b48607f2
Mathew131 <97729815+Mathew131@users.noreply.github.com>
2024-05-23 19:24:31 +0000

    task_06

--- task_06/src/test.cpp
+++ task_06/src/test.cpp
@@ -1,6 +1,21 @@
 
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+#include "task_06.hpp"
+
+TEST(MergeSortTests, Case1) {
+  ASSERT_EQ(findNthOrder(std::vector<int>{73, 74, 75, 71, 69, 72, 76, 73}, 2),
+            71);
+}
+
+TEST(MergeSortTests, Case2) {
+  ASSERT_EQ(findNthOrder(std::vector<int>{30, 40, 50, 60}, 1), 30);
+}
+
+TEST(MergeSortTests, Case3) {
+  ASSERT_EQ(findNthOrder(std::vector<int>{30, 60, 90, 70, 80, 40, 30}, 4), 60);
+}
+
+TEST(MergeSortTests, Case4) {
+  ASSERT_EQ(findNthOrder(std::vector<int>{55, 55, 55, 55, 55}, 5), 55);
 }
