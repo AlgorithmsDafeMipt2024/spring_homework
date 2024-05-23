a8121d82f21e2e7b696a98f39872ff2b4406fdc5
Mathew131 <97729815+Mathew131@users.noreply.github.com>
2024-05-23 16:08:32 +0000

    task_04

--- task_04/src/test.cpp
+++ task_04/src/test.cpp
@@ -1,6 +1,19 @@
 
 #include <gtest/gtest.h>
 
+#include "heap.hpp"
+
 TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+  Heap a;
+  ASSERT_EQ(a.size(), 0);
+  a.Insert(5);
+  ASSERT_EQ(a.GetMin(), 5);
+  a.Insert(3);
+  ASSERT_EQ(a.GetMin(), 3);
+  a.Insert(1);
+  a.Insert(7);
+  a.Insert(-2);
+  a.extractMin();
+  ASSERT_EQ(a.size(), 4);
+  ASSERT_EQ(a.GetMin(), -2);
 }
