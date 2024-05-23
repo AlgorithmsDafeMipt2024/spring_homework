230696f03f266cd36b3422206d7a87f75f0cb9ca
Mathew131 <97729815+Mathew131@users.noreply.github.com>
2024-05-23 19:25:23 +0000

    update task_05

--- task_05/src/sort.hpp
+++ task_05/src/sort.hpp
@@ -2,41 +2,42 @@
 
 #include <vector>
 
-std::vector<int> merge(const std::vector<int>& left, const std::vector<int>& right) {
-    std::vector<int> result;
-    int i = 0, j = 0;
+std::vector<int> merge(const std::vector<int>& left,
+                       const std::vector<int>& right) {
+  std::vector<int> result;
+  int i = 0, j = 0;
 
-    while (i < left.size() && j < right.size()) {
-        if (left[i] <= right[j]) {
-            result.push_back(left[i]);
-            ++i;
-        } else {
-            result.push_back(right[j]);
-            ++j;
-        }
+  while (i < left.size() && j < right.size()) {
+    if (left[i] <= right[j]) {
+      result.push_back(left[i]);
+      ++i;
+    } else {
+      result.push_back(right[j]);
+      ++j;
     }
+  }
 
-    while (i < left.size()) {
-        result.push_back(left[i]);
-        ++i;
-    }
+  while (i < left.size()) {
+    result.push_back(left[i]);
+    ++i;
+  }
 
-    while (j < right.size()) {
-        result.push_back(right[j]);
-        ++j;
-    }
+  while (j < right.size()) {
+    result.push_back(right[j]);
+    ++j;
+  }
 
-    return result;
+  return result;
 }
 
 std::vector<int> mergeSort(std::vector<int> arr) {
-    if (arr.size() <= 1) {
-        return arr;
-    }
+  if (arr.size() <= 1) {
+    return arr;
+  }
 
-    int mid = arr.size() / 2;
-    std::vector<int> left(arr.begin(), arr.begin() + mid);
-    std::vector<int> right(arr.begin() + mid, arr.end());
+  int mid = arr.size() / 2;
+  std::vector<int> left(arr.begin(), arr.begin() + mid);
+  std::vector<int> right(arr.begin() + mid, arr.end());
 
-    return merge(mergeSort(left), mergeSort(right));
+  return merge(mergeSort(left), mergeSort(right));
 }
