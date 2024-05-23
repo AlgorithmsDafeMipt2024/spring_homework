98ce20aced5871f084db27dd3555006144c7da0e
Mathew131 <97729815+Mathew131@users.noreply.github.com>
2024-05-23 15:32:42 +0000

    task_03

--- task_03/src/topology_sort.cpp
+++ task_03/src/topology_sort.cpp
@@ -1,1 +1,16 @@
 #include "topology_sort.hpp"
+
+std::vector<int> Task3(std::vector<int> temperature) {
+  std::vector<int> result;
+  for (int i = 0; i < temperature.size(); ++i) {
+    int count_day = 0;
+    for (int j = i; j < temperature.size(); ++j) {
+      if (temperature[j] <= temperature[i])
+        count_day++;
+      else
+        break;
+    }
+    result.push_back(count_day);
+  }
+  return result;
+}
--- task_03/src/topology_sort.hpp
+++ task_03/src/topology_sort.hpp
@@ -1,1 +1,4 @@
 #pragma once
+#include <vector>
+
+std::vector<int> Task3(std::vector<int>);
