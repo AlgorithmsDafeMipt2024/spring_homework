cbce95c3b3b13da355dbfc7c2323b7763c8beee4
Sukhanov George <sukhanov.ge@phystech.edu>
2024-05-24 02:27:42 +0000

    task_08 done

--- task_08/src/test.cpp
+++ task_08/src/test.cpp
@@ -1,6 +1,54 @@
-
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+#include <hash_table.hpp>
+#include <stdexcept>
+
+TEST(HashTableTest, ContainsElement) {
+  HashTable table;
+  table.Insert(5);
+  ASSERT_TRUE(table.Contains(5));
+}
+
+TEST(HashTableTest, InsertElement) {
+  HashTable table;
+  table.Insert(14);
+  ASSERT_TRUE(table.Contains(14));
+}
+
+TEST(HashTableTest, RemoveElement) {
+  HashTable table;
+  table.Insert(45);
+  table.Remove(45);
+  ASSERT_FALSE(table.Contains(45));
+}
+
+TEST(HashTableTest, ClearTable) {
+  HashTable table;
+  table.Insert(41420);
+  table.Clear();
+  ASSERT_EQ(table.Size(), 0);
+}
+
+TEST(HashTableTest, SizeAfterInsertion) {
+  HashTable table;
+  table.Insert(245);
+  table.Insert(301);
+  ASSERT_EQ(table.Size(), 2);
+}
+
+TEST(HashTableTest, ClearTableWithManyElements) {
+  HashTable table;
+  for (int i = 0; i < 1000; ++i) {
+    table.Insert(i);
+  }
+  table.Clear();
+  ASSERT_EQ(table.Size(), 0);
+}
+
+TEST(HashTableTest, CollisionHandling) {
+  HashTable table;
+  table.Insert(5);
+  table.Insert(15);
+  ASSERT_TRUE(table.Contains(5));
+  ASSERT_TRUE(table.Contains(15));
 }
