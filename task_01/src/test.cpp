0019009add2f52d4f6d85e2eb92fa29ff4e01220
XehiDat <63257446+ZetroRetro@users.noreply.github.com>
2024-06-04 20:10:38 +0000

    another fix

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -6,7 +6,7 @@
 TEST(find_pair_with_sum, Simple) {
   std::vector<int> test1 = {2, 3, 7, 12, 13, 28, 30, 60};
 
-  // ASSERT_EQ(find_pair_with_sum(test1, 14), std::vector<int>(0, 4));
+  ASSERT_EQ(find_pair_with_sum(test1, 14), std::vector<int>({0, 3}));
   ASSERT_EQ(find_pair_with_sum(test1, 20), std::vector<int>({2, 4}));
   ASSERT_EQ(find_pair_with_sum(test1, 21), std::vector<int>({-1, -1}));
   ASSERT_EQ(find_pair_with_sum(test1, 58), std::vector<int>({5, 6}));
