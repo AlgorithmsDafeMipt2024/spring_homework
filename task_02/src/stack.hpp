479288156871ddd9d647c5079ab80189b1c7c8db
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-06-07 22:45:34 +0000

    complete task_02 with tests

--- task_02/src/stack.hpp
+++ task_02/src/stack.hpp
@@ -1,23 +1,81 @@
 #pragma once
 
-#include <stack>
-#include <vector>
+#include <memory>
+#include <stdexcept>
 
+template <typename T>
+struct StackElem {
+  explicit StackElem(T value, const std::shared_ptr<StackElem>& prev)
+      : value_{value}, prev_{prev} {}
+
+  T value_;
+  std::shared_ptr<StackElem> prev_;
+};
+
+template <typename T>
 class Stack {
  public:
-  void Push(int value);
-  int Pop();
+  explicit Stack() : top_{nullptr} {}
+
+  void Push(T value);
+  T Pop();
+
+  T Top() const {
+    if (IsEmpty()) throw std::out_of_range("Stack is empty!");
+    return top_->value_;
+  }
+  size_t Size() const { return size_; }
+  bool IsEmpty() const { return size_ == 0; }
 
  private:
-  std::stack<int> data_;
+  std::shared_ptr<StackElem<T>> top_;
+  size_t size_ = 0;
 };
 
+template <typename T>
 class MinStack {
  public:
-  void Push(int value);
-  int Pop();
-  int GetMin();
+  explicit MinStack() : data_(), mins_data_() {}
+
+  void Push(T value);
+  T Pop();
+
+  T Top() const { return data_.Top(); }
+  T GetMin() const { return mins_data_.Top(); }
+  size_t Size() const { return data_.Size(); }
+  bool IsEmpty() const { return data_.IsEmpty(); }
 
  private:
-  std::vector<int> data_;
+  Stack<T> data_;
+  Stack<T> mins_data_;
 };
+
+template <typename T>
+void Stack<T>::Push(T value) {
+  top_ = std::make_shared<StackElem<T>>(value, top_);
+  size_++;
+}
+
+template <typename T>
+T Stack<T>::Pop() {
+  if (IsEmpty()) throw std::out_of_range("Stack is empty!");
+  T res = top_->value_;
+  top_ = std::move(top_->prev_);
+  size_--;
+  return res;
+}
+
+template <typename T>
+void MinStack<T>::Push(T value) {
+  data_.Push(value);
+  if (mins_data_.Size() > 0 && mins_data_.Top() < value)
+    mins_data_.Push(mins_data_.Top());
+  else
+    mins_data_.Push(value);
+}
+
+template <typename T>
+T MinStack<T>::Pop() {
+  mins_data_.Pop();
+  return data_.Pop();
+}
