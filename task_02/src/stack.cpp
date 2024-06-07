f9c896dc9b185bdc3138d12b6813326799b49653
Lynech <144128009+Lynech@users.noreply.github.com>
2024-06-07 14:08:37 +0000

    taks 02     написал реализацию stack и minstack для int     (тк в постановке задания не уточнено требуется template или нет,     кроме этого нет ребования добавить тесты)

--- task_02/src/stack.cpp
+++ task_02/src/stack.cpp
@@ -1,21 +1,32 @@
 #include "stack.hpp"
 
 #include <algorithm>
+#include <utility>
 
-void Stack::Push(int value) { data_.push(value); }
+void Stack::Push(int value) { data_.push_back(value); }
 
 int Stack::Pop() {
-  auto result = data_.top();
-  data_.pop();
-  return result;
-}
-
-void MinStack::Push(int value) { data_.push_back(value); }
-
-int MinStack::Pop() {
   auto result = data_.back();
   data_.pop_back();
   return result;
 }
 
-int MinStack::GetMin() { return *std::min_element(data_.begin(), data_.end()); }
+void MinStack::Push(int value) {
+  if (GetMin() < value)
+    min_.Push(GetMin());
+  else
+    min_.Push(value);
+  data_.Push(value);
+}
+
+int MinStack::Pop() {
+  auto result = data_.Pop();
+  min_.Pop();
+  return result;
+}
+
+int MinStack::GetMin() {
+  auto result = min_.Pop();
+  min_.Push(result);
+  return result;
+}
--- task_02/src/stack.hpp
+++ task_02/src/stack.hpp
@@ -9,7 +9,7 @@ class Stack {
   int Pop();
 
  private:
-  std::stack<int> data_;
+  std::vector<int> data_;
 };
 
 class MinStack {
@@ -19,5 +19,6 @@ class MinStack {
   int GetMin();
 
  private:
-  std::vector<int> data_;
+  Stack data_;
+  Stack min_;
 };
