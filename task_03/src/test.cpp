570459bf5fd560dce43017fee22c3107695c3b5f
XehiDat <63257446+ZetroRetro@users.noreply.github.com>
2024-06-09 16:50:14 +0000

    task_03 finished

--- task_03/src/test.cpp
+++ task_03/src/test.cpp
@@ -1,8 +1,25 @@
 
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "topology_sort.hpp"
 
 TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+  ASSERT_EQ(find_higher_temp({0, 2, 6, 4, 8, 4, 6, 0, 3, -3, -2, -6, 1}),
+            std::vector<int>({1, 1, 2, 1, 0, 1, 0, 1, 0, 1, 2, 1, 0}));
+  ASSERT_EQ(find_higher_temp({0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0}),
+            std::vector<int>({1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0}));
+  ASSERT_EQ(find_higher_temp({0, 1, -1, 0, 1, -1, 0, 1}),
+            std::vector<int>({1, 0, 1, 1, 0, 1, 1, 0}));
+  ASSERT_EQ(find_higher_temp({1}), std::vector<int>({0}));
+  ASSERT_EQ(find_higher_temp({-1, -1, -1, -3, -3, -3, -2, -2, -2, 0, 0, 0}),
+            std::vector<int>({9, 8, 7, 3, 2, 1, 3, 2, 1, 0, 0, 0}));
+  ASSERT_EQ(find_higher_temp({0, 0, 0, 0, 0, 0, 0}),
+            std::vector<int>({0, 0, 0, 0, 0, 0, 0}));
+            ASSERT_EQ(find_higher_temp({}),
+            std::vector<int>({}));
+  ASSERT_EQ(find_higher_temp({1, 5, 2, 3, 4, 0}),
+            std::vector<int>({1, 0, 1, 1, 0, 0}));
+            
 }
