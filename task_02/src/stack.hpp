8d5137e1702f8e5bf4178134ef66083ed3861efa
MRyabchikov <110342451+MRyabchikov@users.noreply.github.com>
2024-06-11 12:24:04 +0000

    Task2

--- task_02/src/stack.hpp
+++ task_02/src/stack.hpp
@@ -1,23 +1,103 @@
 #pragma once
 
+#include <cstddef>
+#include <memory>
 #include <stack>
+#include <stdexcept>
+#include <type_traits>
 #include <vector>
 
+template <class T>
+class StackNode {
+ public:
+  explicit StackNode(T _data, StackNode<T> *_prev = nullptr)
+      : data(_data), prevptr(_prev) {}
+  StackNode() : data(), prevptr(nullptr) {}
+  T data;
+  StackNode<T> *prevptr;
+};
+
+template <class T>
 class Stack {
  public:
-  void Push(int value);
-  int Pop();
+  Stack() : top(nullptr), size(0) {}
+  void push(T value);
+  T pop();
+  T getTop();
+  int getSize();
 
  private:
-  std::stack<int> data_;
+  StackNode<T> *top;
+  int size;
 };
 
+template <class T>
+void Stack<T>::push(T value) {
+  StackNode<T> *newElem = new StackNode<T>(value, top);
+  size++;
+  top = newElem;
+}
+
+template <class T>
+T Stack<T>::pop() {
+  if (size == 0) {
+    throw std::out_of_range("Empty stack");
+  }
+  T returningValue = top->data;
+  StackNode<T> *oldTop = top;
+  top = top->prevptr;
+  delete oldTop;
+  size--;
+  return returningValue;
+}
+
+template <class T>
+int Stack<T>::getSize() {
+  return size;
+}
+
+template <class T>
+T Stack<T>::getTop() {
+  if (size == 0) {
+    throw std::out_of_range("Empty stack");
+  }
+  return top->data;
+}
+template <class T>
 class MinStack {
  public:
-  void Push(int value);
-  int Pop();
-  int GetMin();
+  MinStack() : mainStack(), minStack() {}
+  void push(T value);
+  T pop();
+  T getMin();
+  int getSize();
 
  private:
-  std::vector<int> data_;
+  Stack<T> mainStack;
+  Stack<T> minStack;
 };
+
+template <class T>
+void MinStack<T>::push(T value) {
+  mainStack.push(value);
+  if (minStack.getSize() == 0)
+    minStack.push(value);
+  else
+    minStack.push(std::min(value, minStack.getTop()));
+}
+
+template <class T>
+T MinStack<T>::pop() {
+  minStack.pop();
+  return mainStack.pop();
+}
+
+template <class T>
+T MinStack<T>::getMin() {
+  return minStack.getTop();
+}
+
+template <class T>
+int MinStack<T>::getSize() {
+  return mainStack.getSize();
+}
--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -6,37 +6,37 @@
 #include "stack.hpp"
 
 TEST(StackTest, Simple) {
-  Stack stack;
-  stack.Push(1);              // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  stack.Push(3);              // Stack [1, 3]
-  ASSERT_EQ(stack.Pop(), 3);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
+  Stack<int> stack;
+  stack.push(1);              // Stack [1]
+  ASSERT_EQ(stack.pop(), 1);  // Stack []
+  stack.push(1);              // Stack [1]
+  stack.push(2);              // Stack [1, 2]
+  ASSERT_EQ(stack.pop(), 2);  // Stack [1]
+  ASSERT_EQ(stack.pop(), 1);  // Stack []
+  stack.push(1);              // Stack [1]
+  stack.push(2);              // Stack [1, 2]
+  ASSERT_EQ(stack.pop(), 2);  // Stack [1]
+  stack.push(3);              // Stack [1, 3]
+  ASSERT_EQ(stack.pop(), 3);  // Stack [1]
+  ASSERT_EQ(stack.pop(), 1);  // Stack []
 }
 
 TEST(MinStackTest, Simple) {
-  MinStack stack;
-  stack.Push(1);  // Stack [1]
-  ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
-  stack.Push(1);              // Stack [1]
-  stack.Push(2);              // Stack [1, 2]
-  ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 2);  // Stack [1]
-  stack.Push(3);              // Stack [1, 3]
-  ASSERT_EQ(stack.GetMin(), 1);
-  ASSERT_EQ(stack.Pop(), 3);  // Stack [1]
-  ASSERT_EQ(stack.Pop(), 1);  // Stack []
+  MinStack<int> stack;
+  stack.push(1);  // Stack [1]
+  ASSERT_EQ(stack.getMin(), 1);
+  ASSERT_EQ(stack.pop(), 1);  // Stack []
+  stack.push(1);              // Stack [1]
+  stack.push(2);              // Stack [1, 2]
+  ASSERT_EQ(stack.getMin(), 1);
+  ASSERT_EQ(stack.pop(), 2);  // Stack [1]
+  ASSERT_EQ(stack.pop(), 1);  // Stack []
+  stack.push(1);              // Stack [1]
+  stack.push(2);              // Stack [1, 2]
+  ASSERT_EQ(stack.getMin(), 1);
+  ASSERT_EQ(stack.pop(), 2);  // Stack [1]
+  stack.push(3);              // Stack [1, 3]
+  ASSERT_EQ(stack.getMin(), 1);
+  ASSERT_EQ(stack.pop(), 3);  // Stack [1]
+  ASSERT_EQ(stack.pop(), 1);  // Stack []
 }
