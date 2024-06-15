13266731cfa98498bd2a375c017a267f8622eba7
Sukhanov George <sukhanov.ge@phystech.edu>
2024-06-15 11:59:10 +0000

    added test

--- task_08/src/test.cpp
+++ task_08/src/test.cpp
@@ -52,3 +52,10 @@ TEST(HashTableTest, CollisionHandling) {
   ASSERT_TRUE(table.Contains(5));
   ASSERT_TRUE(table.Contains(15));
 }
+TEST(HashTableTest, ADD100Delete50Rehash) {
+  HashTable table;
+  for (int i = 0; i < 100; i++) table.Insert(i);
+  for (int i = 0; i < 50; i++) table.Remove(100 - i);
+  for (int i = 0; i < 38; i++) table.Insert(i);
+  ASSERT_EQ(table.Size(), 39);
+}
