c7063ba6e8d454d214b3e8bde86381ca07525c82
Mathew131 <97729815+Mathew131@users.noreply.github.com>
2024-06-15 11:44:20 +0000

    task_02

--- task_02/src/stack.cpp
+++ task_02/src/stack.cpp
@@ -2,20 +2,48 @@
 
 #include <algorithm>
 
-void Stack::Push(int value) { data_.push(value); }
+void Stack::Push(int value) { elements_.push_back(value); }
 
 int Stack::Pop() {
-  auto result = data_.top();
-  data_.pop();
+  if (elements_.size() == 0) {
+    throw std::out_of_range("Stack is empty");
+  }
+  int result = elements_.back();
+  elements_.pop_back();
   return result;
 }
 
-void MinStack::Push(int value) { data_.push_back(value); }
+int Stack::Size() { return elements_.size(); }
+
+int Stack::Top() {
+  if (elements_.size() == 0) {
+    throw std::out_of_range("Stack is empty");
+  }
+  return elements_.back();
+}
+
+///////////////////
+
+void MinStack::Push(int value) {
+  main_stack_.Push(value);
+  if (min_stack_.Size() && min_stack_.Top() > value) {
+    min_stack_.Push(value);
+  }
+  if (min_stack_.Size() == 0) {
+    min_stack_.Push(value);
+  }
+}
 
 int MinStack::Pop() {
-  auto result = data_.back();
-  data_.pop_back();
-  return result;
+  if (main_stack_.Size() && min_stack_.Size() &&
+      main_stack_.Top() == min_stack_.Top()) {
+    min_stack_.Pop();
+  }
+  return main_stack_.Pop();
 }
 
-int MinStack::GetMin() { return *std::min_element(data_.begin(), data_.end()); }
+int MinStack::GetMin() { return min_stack_.Top(); }
+
+int MinStack::Top() { return main_stack_.Top(); }
+
+int MinStack::Size() { return main_stack_.Size(); }
