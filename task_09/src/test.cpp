d592237547667cd3637bd30df54f1214e73045e8
Mathew131 <97729815+Mathew131@users.noreply.github.com>
2024-06-15 17:43:02 +0000

    add new test in task_09

--- task_09/src/test.cpp
+++ task_09/src/test.cpp
@@ -8,3 +8,5 @@ TEST(MinCoins, 1) { ASSERT_EQ(MinCoins(std::vector<int>{1, 2, 5, 10}, 14), 3); }
 TEST(MinCoins, 2) { ASSERT_EQ(MinCoins(std::vector<int>{1, 2, 5, 10}, 19), 4); }
 
 TEST(MinCoins, 3) { ASSERT_EQ(MinCoins(std::vector<int>{2, 4, 6}, 13), -1); }
+
+TEST(MinCoins, 4) { ASSERT_EQ(MinCoins(std::vector<int>{3, 4, 5, 10}, 17), 3); }
