45de90fd8e541f3967fad9111e6e627fb5fb58dc
Sukhanov George <sukhanov.ge@phystech.edu>
2024-06-15 11:54:39 +0000

    fix private field name

--- task_02/src/list_stacks.hpp
+++ task_02/src/list_stacks.hpp
@@ -7,59 +7,59 @@ template <typename T>
 class Stack final {
  public:
   constexpr Stack() noexcept {}
-  Stack(const Stack& other) : list(other.list) {}
-  Stack(Stack&& other) : list(other.list) { other.list.clear(); }
-  Stack(const std::initializer_list<T>& init_list) : list(init_list) {}
+  Stack(const Stack& other) : list_(other.list_) {}
+  Stack(Stack&& other) : list_(std::move(other.list_)) { other.list_.clear(); }
+  Stack(const std::initializer_list<T>& init_list) : list_(init_list) {}
 
   ~Stack() = default;
 
-  void Push(const T& value) { list.push_back(value); }
+  void Push(const T& value) { list_.push_back(value); }
   void Pop() {
     if (Empty()) throw std::underflow_error("Stack is empty!");
-    list.pop_back();
+    list_.pop_back();
   }
 
   T& Top() {
     if (Empty()) throw std::underflow_error("Stack is empty!");
-    return list.back();
+    return list_.back();
   }
-  bool Empty() { return list.empty(); }
-  std::size_t Size() { return list.size(); }
+  bool Empty() { return list_.empty(); }
+  std::size_t Size() { return list_.size(); }
 
  private:
-  std::list<T> list;
+  std::list<T> list_;
 };
 
 template <typename T>
 class StackMin final {
  public:
   constexpr StackMin() noexcept {}
-  StackMin(const StackMin& other) : list(other.list) {}
-  StackMin(StackMin&& other) : list(other.list) { other.list.clear(); }
-  StackMin(const std::initializer_list<T>& init_list) : list(init_list) {}
+  StackMin(const StackMin& other) : list_(other.list_) {}
+  StackMin(StackMin&& other) : list_(other.list_) { other.list_.clear(); }
+  StackMin(const std::initializer_list<T>& init_list_) : list_(init_list_) {}
 
   ~StackMin() = default;
 
   void Push(const T& value) {
     if (min_stack.Empty() || min_stack.Top() > value) min_stack.Push(value);
-    list.push_back(value);
+    list_.push_back(value);
   }
   void Pop() {
     if (Empty()) throw std::underflow_error("Stack is empty!");
-    if (min_stack.Top() == list.back()) min_stack.Pop();
-    list.pop_back();
+    if (min_stack.Top() == list_.back()) min_stack.Pop();
+    list_.pop_back();
   }
 
   T& Top() {
     if (Empty()) throw std::underflow_error("Stack is empty!");
-    return list.back();
+    return list_.back();
   }
-  bool Empty() { return list.empty(); }
-  std::size_t Size() { return list.size(); }
+  bool Empty() { return list_.empty(); }
+  std::size_t Size() { return list_.size(); }
 
   T& Min() { return min_stack.Top(); }
 
  private:
-  std::list<T> list;
+  std::list<T> list_;
   Stack<T> min_stack;
 };
