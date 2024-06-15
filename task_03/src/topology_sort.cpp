0619d9d76bfd4ad5ddb686b031e6039a0a9f2ce9
Mathew131 <97729815+Mathew131@users.noreply.github.com>
2024-06-15 09:00:16 +0000

    final update task_03

--- task_03/src/topology_sort.cpp
+++ task_03/src/topology_sort.cpp
@@ -1,7 +1,7 @@
 #include "topology_sort.hpp"
 
 std::vector<int> TopologySort(std::vector<int> temperature) {
-  std::vector<int> result(0, temperature.size());
+  std::vector<int> result(temperature.size(), 0);
   std::stack<int> index;
 
   for (int i = 0; i < temperature.size(); ++i) {
@@ -11,9 +11,6 @@ std::vector<int> TopologySort(std::vector<int> temperature) {
       result[current_index] = i - current_index;
     }
     index.push(i);
-  }
-  for (int i = 0; i < index.size(); ++i) {
-    result.push_back(0);
   }
 
   return result;
