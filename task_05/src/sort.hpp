69f7a5ff940a3c7e401b6ba3135940f305874976
Mathew131 <97729815+Mathew131@users.noreply.github.com>
2024-06-15 09:44:31 +0000

    update task_05

--- task_05/src/sort.hpp
+++ task_05/src/sort.hpp
@@ -2,7 +2,7 @@
 
 #include <vector>
 
-std::vector<int> merge(const std::vector<int>& left,
+std::vector<int> Merge(const std::vector<int>& left,
                        const std::vector<int>& right) {
   std::vector<int> result;
   int i = 0, j = 0;
@@ -30,7 +30,7 @@ std::vector<int> merge(const std::vector<int>& left,
   return result;
 }
 
-std::vector<int> mergeSort(std::vector<int> arr) {
+std::vector<int> MergeSort(std::vector<int> arr) {
   if (arr.size() <= 1) {
     return arr;
   }
@@ -39,5 +39,5 @@ std::vector<int> mergeSort(std::vector<int> arr) {
   std::vector<int> left(arr.begin(), arr.begin() + mid);
   std::vector<int> right(arr.begin() + mid, arr.end());
 
-  return merge(mergeSort(left), mergeSort(right));
+  return Merge(MergeSort(left), MergeSort(right));
 }
--- task_05/src/test.cpp
+++ task_05/src/test.cpp
@@ -4,31 +4,36 @@
 #include "sort.hpp"
 
 TEST(MergeSortTests, Case1) {
-  ASSERT_EQ(mergeSort(std::vector<int>{73, 74, 75, 71, 69, 72, 76, 73}),
+  ASSERT_EQ(MergeSort(std::vector<int>{73, 74, 75, 71, 69, 72, 76, 73}),
             (std::vector<int>{69, 71, 72, 73, 73, 74, 75, 76}));
 }
 
 TEST(MergeSortTests, Case2) {
-  ASSERT_EQ(mergeSort(std::vector<int>{30, 40, 50, 60}),
+  ASSERT_EQ(MergeSort(std::vector<int>{30, 40, 50, 60}),
             (std::vector<int>{30, 40, 50, 60}));
 }
 
 TEST(MergeSortTests, Case3) {
-  ASSERT_EQ(mergeSort(std::vector<int>{30, 60, 90, 70, 80, 40, 30}),
+  ASSERT_EQ(MergeSort(std::vector<int>{30, 60, 90, 70, 80, 40, 30}),
             (std::vector<int>{30, 30, 40, 60, 70, 80, 90}));
 }
 
 TEST(MergeSortTests, Case4) {
-  ASSERT_EQ(mergeSort(std::vector<int>{55, 55, 55, 55, 55}),
+  ASSERT_EQ(MergeSort(std::vector<int>{55, 55, 55, 55, 55}),
             (std::vector<int>{55, 55, 55, 55, 55}));
 }
 
 TEST(MergeSortTests, Case5) {
-  ASSERT_EQ(mergeSort(std::vector<int>{76, 75, 74, 73, 72, 71, 70}),
+  ASSERT_EQ(MergeSort(std::vector<int>{76, 75, 74, 73, 72, 71, 70}),
             (std::vector<int>{70, 71, 72, 73, 74, 75, 76}));
 }
 
 TEST(MergeSortTests, Case6) {
-  ASSERT_EQ(mergeSort(std::vector<int>{75, 72, 71, 70, 74, 76}),
+  ASSERT_EQ(MergeSort(std::vector<int>{75, 72, 71, 70, 74, 76}),
             (std::vector<int>{70, 71, 72, 74, 75, 76}));
+}
+
+TEST(MergeSortTests, Case7) {
+  ASSERT_EQ(MergeSort(std::vector<int>{}),
+            (std::vector<int>{}));
 }
