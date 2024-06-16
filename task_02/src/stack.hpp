ade8343e7ba56ff2729370e6a9eb33664a3dcf90
sh1shk1nsky <144131539+sh1shk1nsky@users.noreply.github.com>
2024-06-16 23:36:10 +0000

    task 2(first attemp)

--- task_02/src/stack.hpp
+++ task_02/src/stack.hpp
@@ -1,23 +1,59 @@
-#pragma once
-
-#include <stack>
+#ifndef STACK_HPP
+#define STACK_HPP
+#include <exception>
+#include <optional>
+#include <stdexcept>
 #include <vector>
 
-class Stack {
+template <class T>
+class stack {
  public:
-  void Push(int value);
-  int Pop();
+  void push(T value) { data_.push_back(value); }
+  T pop() {
+    if (data_.empty()) throw std::runtime_error("Stack is empty");
+    int result = data_.back();
+    data_.pop_back();
+    return result;
+  }
 
  private:
-  std::stack<int> data_;
+  std::vector<T> data_;
 };
 
-class MinStack {
+template <class T>
+class minStack {
  public:
-  void Push(int value);
-  int Pop();
-  int GetMin();
+  void push(T value) {
+    if ((getMin_()) && *getMin_() < value)
+      min_.push(getMin());
+    else
+      min_.push(value);
+    data_.push(value);
+  }
+  T pop() {
+    T result = data_.pop();
+    min_.pop();
+    return result;
+  }
+  T getMin() {
+    T result = min_.pop();
+    min_.push(result);
+    return result;
+  }
 
  private:
-  std::vector<int> data_;
+  std::optional<T> getMin_() {
+    std::optional<T> result;
+    try {
+      result = min_.pop();
+      min_.push(*result);
+    } catch (std::exception& e) {
+      result = std::nullopt;
+    }
+    return result;
+  }
+  stack<T> data_;
+  stack<T> min_;
 };
+
+#endif
