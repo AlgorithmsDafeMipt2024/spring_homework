a66774fd540b0c5e5a73b8953778f3817cce6cf8
Lynech <144128009+Lynech@users.noreply.github.com>
2024-06-16 14:11:45 +0000

    task_09

--- task_09/src/test.cpp
+++ task_09/src/test.cpp
@@ -1,4 +1,21 @@
 
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) { ASSERT_EQ(1, 1); }
+#include <stdexcept>
+#include <vector>
+
+#include "money_to_coins.hpp"
+
+TEST(MoneyToCoins, Simple) {
+  ASSERT_EQ(MoneyToCoins(14, std::vector<size_t>{1, 2, 5, 10}), 3);
+  ASSERT_EQ(MoneyToCoins(19, std::vector<size_t>{1, 2, 5, 10}), 4);
+  ASSERT_EQ(MoneyToCoins(15, std::vector<size_t>{1, 7, 10}), 3);
+  ASSERT_EQ(MoneyToCoins(14, std::vector<size_t>{1, 7, 10}), 2);
+  ASSERT_EQ(MoneyToCoins(21, std::vector<size_t>{1, 7, 10}), 3);
+  ASSERT_EQ(MoneyToCoins(34, std::vector<size_t>{1, 7, 10}), 4);
+}
+
+TEST(MoneyToCoins, Empty) {
+  ASSERT_THROW(MoneyToCoins(14, std::vector<size_t>{}), std::runtime_error);
+  ASSERT_EQ(MoneyToCoins(0, std::vector<size_t>{1, 2, 5, 10}), 0);
+}
