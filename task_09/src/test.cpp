7ecdbf3679f04c507c7dff6e0d14d004f1433061
Ilya_Rybalkin <rybalkin.id@phystech.edu>
2024-06-16 19:55:46 +0000

    beautify

--- task_09/src/test.cpp
+++ task_09/src/test.cpp
@@ -3,12 +3,14 @@
 #include "min_amount_of_coins.h"
 
 TEST(MinAmountOfCoins, Simple) {
-  ASSERT_EQ(MinAmountOfCoins(14, std::vector<size_t>{1, 2, 5, 10}), 3);
-  ASSERT_EQ(MinAmountOfCoins(19, std::vector<size_t>{1, 2, 5, 10}), 4);
-  ASSERT_EQ(MinAmountOfCoins(15, std::vector<size_t>{1, 7, 10}), 3);
-  ASSERT_EQ(MinAmountOfCoins(14, std::vector<size_t>{1, 7, 10}), 2);
-  ASSERT_EQ(MinAmountOfCoins(21, std::vector<size_t>{1, 7, 10}), 3);
-  ASSERT_EQ(MinAmountOfCoins(34, std::vector<size_t>{1, 7, 10}), 4);
+  std::vector<size_t> coins_1{1, 2, 5, 10};
+  std::vector<size_t> coins_2{1, 7, 10};
+  ASSERT_EQ(MinAmountOfCoins(14, coins_1), 3);
+  ASSERT_EQ(MinAmountOfCoins(19, coins_1), 4);
+  ASSERT_EQ(MinAmountOfCoins(15, coins_2), 3);
+  ASSERT_EQ(MinAmountOfCoins(14, coins_2), 2);
+  ASSERT_EQ(MinAmountOfCoins(21, coins_2), 3);
+  ASSERT_EQ(MinAmountOfCoins(34, coins_2), 4);
   ASSERT_THROW(MinAmountOfCoins(14, std::vector<size_t>{}), std::runtime_error);
-  ASSERT_EQ(MinAmountOfCoins(0, std::vector<size_t>{1, 2, 5, 10}), 0);
+  ASSERT_EQ(MinAmountOfCoins(0, coins_1), 0);
 }
