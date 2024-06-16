3c8df38d1a4b57506f8b69363d8809e887282ebf
sh1shk1nsky <144131539+sh1shk1nsky@users.noreply.github.com>
2024-06-16 20:47:15 +0000

    update tests

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -1,15 +1,19 @@
 #include <gtest/gtest.h>
 
-#include "topology_sort.hpp"
+#include "utils.hpp"
 
 TEST(MySearch, Simple) {
-  std::vector<int> list = {2, 4, 7, 9, 12, 16, 19, 23, 27};
-  ASSERT_EQ(MySearch(list, 50), std::tuple(23, 27));
-  ASSERT_EQ(MySearch(list, 43), std::tuple(16, 27));
-  ASSERT_EQ(MySearch(list, 47), std::tuple(-1, -1));
-  ASSERT_EQ(MySearch(list, 24), std::tuple(-1, -1));
-  ASSERT_EQ(MySearch(list, 21), std::tuple(2, 19));
-  ASSERT_EQ(MySearch(list, 18), std::tuple(2, 16));
-  ASSERT_EQ(MySearch(list, 11), std::tuple(2, 9));
-  ASSERT_EQ(MySearch(list, 2), std::tuple(-1, -1));
+  std::vector<LL> list = {2, 4, 7, 9, 12, 16, 19, 23, 27};
+  ASSERT_EQ(MySearch(list, 50), std::pair(23LL, 27LL));
+  ASSERT_EQ(MySearch(list, 43), std::pair(16LL, 27LL));
+  ASSERT_EQ(MySearch(list, 21), std::pair(2LL, 19LL));
+  ASSERT_EQ(MySearch(list, 18), std::pair(2LL, 16LL));
+  ASSERT_EQ(MySearch(list, 11), std::pair(2LL, 9LL));
+  ASSERT_EQ(MySearch(list, 47), std::nullopt);
+  ASSERT_EQ(MySearch(list, 24), std::nullopt);
+  ASSERT_EQ(MySearch(list, 2), std::nullopt);
+  ASSERT_EQ(MySearch(list, 1), std::nullopt);
+  ASSERT_EQ(MySearch(list, -1421532541), std::nullopt);
+  list = {};
+  ASSERT_EQ(MySearch(list, 10), std::nullopt);
 }
