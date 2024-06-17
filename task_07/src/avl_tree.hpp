a421386750f9ca42ee9b0078616d6818652c0aa5
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-06-17 13:25:55 +0000

    create primary AVLTree

--- task_07/src/avl_tree.hpp
+++ task_07/src/avl_tree.hpp
@@ -24,3 +24,109 @@ struct AVLTreeNode {
     delete right;
   }
 };
+
+template <typename T>
+class AVLTree {
+ public:
+  AVLTree() : root{nullptr} {}
+
+  bool Contains(int k) const { return Find(root, k) ? true : false; }
+
+  void Push(int k) { root = Insert(root, k); }
+
+  void Pop(int k) { root = Remove(root, k); }
+
+  ~AVLTree() { delete root; }
+
+ private:
+  AVLTreeNode<T>* root;
+
+  AVLTreeNode<T>* RotateRight(AVLTreeNode<T>* n) {
+    AVLTreeNode<T>* l = n->left;
+    n->left = l->right;
+    l->right = n;
+    UpdateHeight(n);
+    UpdateHeight(l);
+
+    return l;
+  }
+
+  AVLTreeNode<T>* RotateLeft(AVLTreeNode<T>* n) {
+    AVLTreeNode<T>* r = n->right;
+    n->right = r->left;
+    r->left = n;
+    UpdateHeight(r);
+    UpdateHeight(n);
+
+    return r;
+  }
+
+  AVLTreeNode<T>* Balance(AVLTreeNode<T>* n) {
+    UpdateHeight(n);
+
+    switch (BalanceFactor(n)) {
+      case 2:
+        if (BalanceFactor(n->right) < 0) n->right = RotateRight(n->right);
+        return RotateLeft(n);
+
+      case -2:
+        if (BalanceFactor(n->left) > 0) n->left = RotateLeft(n->left);
+        return RotateRight(n);
+
+      default:
+        return n;
+    }
+  }
+
+  AVLTreeNode<T>* Insert(AVLTreeNode<T>* n, T k) {
+    if (!n) return new AVLTreeNode(k);
+
+    if (k < n->key)
+      n->left = Insert(n->left, k);
+    else
+      n->right = Insert(n->right, k);
+
+    return Balance(n);
+  }
+
+  AVLTreeNode<T>* FindMin(AVLTreeNode<T>* n) {
+    return n->left ? FindMin(n->left) : n;
+  }
+
+  AVLTreeNode<T>* RemoveMin(AVLTreeNode<T>* n) {
+    if (!n->left) return n->right;
+
+    n->left = RemoveMin(n->left);
+    return Balance(n);
+  }
+
+  AVLTreeNode<T>* Remove(AVLTreeNode<T>* n, T k) {
+    if (!n) return nullptr;
+
+    if (k == n->key) {
+      if (!n->right) return n->left;
+
+      FindMin(n->right)->right = RemoveMin(n->right);
+      FindMin(n->right)->left = n->left;
+      return Balance(FindMin(n->right));
+    }
+
+    k < n->key ? n->left = Remove(n->left, k) : n->right = Remove(n->right, k);
+    return Balance(n);
+  }
+
+  AVLTreeNode<T>* Find(AVLTreeNode<T>* n, T k) const {
+    if (n == nullptr || n->key == k) return n;
+
+    return k < n->key ? Find(n->left, k) : Find(n->right, k);
+  }
+
+  long long BalanceFactor(AVLTreeNode<T>* n) {
+    return n->right->SubtreeHeight() - n->left->SubtreeHeight();
+  }
+
+  void UpdateHeight(AVLTreeNode<T>* n) {
+    n->height =
+        std::max(n->left->SubtreeHeight(), n->right->SubtreeHeight()) + 1;
+  }
+};
