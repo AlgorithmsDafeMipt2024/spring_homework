bfa0486d0c69374212a93da157478c72b7b1cb44
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2024-06-17 14:14:38 +0000

    include fix

--- task_07/src/main.cpp
+++ task_07/src/main.cpp
@@ -1,3 +1,5 @@
 #include <iostream>
 
+#include "avl_tree.hpp"
+
 int main() { return 0; }
--- task_07/src/test.cpp
+++ task_07/src/test.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 
-#include <avl_tree.hpp>
-#include <vector>
+#include "avl_tree.hpp"
 
 TEST(AVLTree, Simple) {
   AVLTree<short> tree;
